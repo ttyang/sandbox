b1e97a7c382ab8930d35984a2a24c75fab14f413
Julio Merino <julio+github@meroh.net>
2006-07-10 17:00:02 +0000

    Preliminary implementation of the Launcher, Child and Status concepts. This is still *very* incomplete and highly subject to change but some functionality starts to work.  I'm commiting this to be able to continue development incrementally in a controlled environment and to avoid losing what already works.  I've been working on this code offline for several days.
    Note that one of the unit tests does not pass.  This is because the user
    has no way to close a communication pipe with a chile process, effectively
    being unable to send an EOF to, e.g., stdin.  In order to fix this I have
    to change multiple things that I'd rather do with the current code in a
    safe place.

--- sandbox/SOC/soc-import/boost/soc/2006/process/trunk/boost/process/detail/pipe.hpp
+++ sandbox/SOC/soc-import/boost/soc/2006/process/trunk/boost/process/detail/pipe.hpp
@@ -13,11 +13,37 @@
 #define BOOST_PROCESS_DETAIL_PIPE_HPP
 
 #include <boost/process/config.hpp>
+#include <boost/shared_ptr.hpp>
 
 #if defined(BOOST_PROCESS_WIN32_API)
 #   include <boost/process/detail/pipe_win32.hpp>
 #else
 #   include <boost/process/detail/pipe_posix.hpp>
 #endif
+
+namespace boost {
+namespace process {
+namespace detail {
+
+// ------------------------------------------------------------------------
+
+// shared_pipe is a wrapper around the pipe class to allow its placement
+// in containers that require to copy objects.
+// XXX: Should the pipe class be reference counted instead?
+class shared_pipe :
+    public boost::shared_ptr< detail::pipe >
+{
+public:
+    shared_pipe(void) :
+        boost::shared_ptr< detail::pipe >(new detail::pipe())
+    {
+    }
+};
+
+// ------------------------------------------------------------------------
+
+} // namespace detail
+} // namespace process
+} // namespace boost
 
 #endif // !defined(BOOST_PROCESS_DETAIL_PIPE_HPP)
