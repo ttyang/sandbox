b4d334fff29c3f2d529af31d2e1d224564f2a105
Matias Capeletto <matias.capeletto@gmail.com>
2006-07-20 16:12:42 +0000

    in the middle of structural changes

---
+++ sandbox/SOC/soc-import/boost/soc/2006/misc/trunk/libs/bimap/test/test_bimap_set_of.cpp
@@ -0,0 +1,20 @@
+// Boost.Bimap
+//
+// (C) Copyright 2006 Matias Capeletto
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// See http://www.boost.org/libs/test for the library home page.
+
+// Boost.Test
+#include <boost/test/minimal.hpp>
+
+#include <boost/bimap/set_of.hpp>
+
+int test_main( int, char* [] )
+{
+    typedef boost::bimap::set_of<int> set_type;
+
+    return 0;
+}
+
