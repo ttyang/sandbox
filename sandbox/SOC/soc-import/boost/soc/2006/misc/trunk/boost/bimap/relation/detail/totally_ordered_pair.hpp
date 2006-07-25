7425c07942fe4dc84d4e202288e81bf84c603f53
Matias Capeletto <matias.capeletto@gmail.com>
2006-07-25 03:25:08 +0000

    VS8 supported

---
+++ sandbox/SOC/soc-import/boost/soc/2006/misc/trunk/boost/bimap/relation/detail/totally_ordered_pair.hpp
@@ -0,0 +1,60 @@
+// Boost.bimap
+//
+// Copyright 2006 Matias Capeletto
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/bimap for library home page.
+
+/// \file relation/detail/totally_ordered_pair.hpp
+/// \brief Macro to help build totally ordered pairs.
+
+#ifndef BOOST_BIMAP_RELATION_DETAIL_TOTALLY_ORDERED_PAIR
+#define BOOST_BIMAP_RELATION_DETAIL_TOTALLY_ORDERED_PAIR
+
+/****************************************************************************/
+
+#define BOOST_BIMAP_TOTALLY_ORDERED_PAIR_IMPLEMENTATION(                     \
+        A_FIRST, A_SECOND,                                                   \
+        B_TYPE,                                                              \
+        B_FIRST, B_SECOND                                                    \
+    )                                                                        \
+                                                                             \
+    bool operator==(const B_TYPE & b) const                                  \
+    {                                                                        \
+        return ( ( A_FIRST  == b.B_FIRST  ) &&                               \
+                 ( A_SECOND == b.B_SECOND ) );                               \
+    }                                                                        \
+                                                                             \
+    bool operator!=(const B_TYPE & b) const                                  \
+    {                                                                        \
+        return ( ( A_FIRST  != b.B_FIRST  ) ||                               \
+                 ( A_SECOND != b.B_SECOND ) );                               \
+    }                                                                        \
+                                                                             \
+    bool operator< (const B_TYPE & b) const                                  \
+    {                                                                        \
+        return ( ( A_FIRST  <  b.B_FIRST  ) &&                               \
+                 ( A_SECOND <  b.B_SECOND ) );                               \
+    }                                                                        \
+                                                                             \
+    bool operator<=(const B_TYPE & b) const                                  \
+    {                                                                        \
+        return ( *this < b || *this == b );                                  \
+    }                                                                        \
+                                                                             \
+    bool operator> (const B_TYPE & b) const                                  \
+    {                                                                        \
+        return ! (*this <= b);                                               \
+    }                                                                        \
+                                                                             \
+    bool operator>=(const B_TYPE & b) const                                  \
+    {                                                                        \
+        return ( *this > b || *this == b );                                  \
+    }
+
+/****************************************************************************/
+
+#endif // BOOST_BIMAP_RELATION_DETAIL_TOTALLY_ORDERED_PAIR
+
