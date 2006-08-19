deab7467e90789c07db0c962e408cccbbcf7a680
Matias Capeletto <matias.capeletto@gmail.com>
2006-08-19 20:06:56 +0000

    unconstrained sets support

--- sandbox/SOC/soc-import/boost/soc/2006/misc/trunk/libs/bimap/test/test_bimap_unconstrained.cpp
+++ sandbox/SOC/soc-import/boost/soc/2006/misc/trunk/libs/bimap/test/test_bimap_unconstrained.cpp
@@ -31,13 +31,66 @@
 // Boost.Test
 #include <boost/test/minimal.hpp>
 
-#include <boost/bimap/set_of.hpp>
+// Boost.Bimap
+#include <boost/bimap/support/lambda.hpp>
+#include <boost/bimap/bimap.hpp>
+
+
+void test_bimap_unconstrained()
+{
+    using namespace boost::bimap;
+
+    {
+        typedef bimap<int,double,unconstrained_set_of_relation> bm;
+        bm b;
+        b.left[2] = 34.4;
+        b.right[2.2] = 3;
+    }
+
+    {
+        typedef bimap<int,unconstrained_set_of<double> > bm;
+        bm b;
+        b.left[2] = 34.4;
+        BOOST_CHECK( b.size() == 1 );
+    }
+
+    {
+        typedef bimap<unconstrained_set_of<int>, double > bm;
+        bm b;
+        b.right[2.4] = 34;
+    }
+
+    {
+        typedef bimap<unconstrained_set_of<int>, double, right_based > bm;
+        bm b;
+        b.right[2.4] = 34;
+        BOOST_CHECK( b.size() == 1 );
+    }
+
+    {
+        typedef bimap<int,unconstrained_set_of<double>, unconstrained_set_of_relation > bm;
+        bm b;
+        b.left[2] = 34.4;
+    }
+
+    {
+        typedef bimap<unconstrained_set_of<int>, double, unconstrained_set_of_relation > bm;
+        bm b;
+        b.right[2.4] = 34;
+    }
+
+    {
+        typedef bimap<unconstrained_set_of<int>, unconstrained_set_of<double>, set_of_relation<> > bm;
+        bm b;
+        b.insert( bm::relation(1,2.3) );
+        BOOST_CHECK( b.size() == 1 );
+    }
+}
+
 
 int test_main( int, char* [] )
 {
-    typedef boost::bimap::set_of<int>       set_type;
-    typedef boost::bimap::set_of_relation<> set_type_of_relation;
-
+    test_bimap_unconstrained();
     return 0;
 }
 
