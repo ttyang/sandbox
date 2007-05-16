f9bb0c71a083acefc1d8807e58c5184233ae6814
Andrew Sutton <andrew.n.sutton@gmail.com>
2007-05-16 14:13:07 +0000

    added, but have not tested the directed graph class.
    started building the kevin bacon example into a suite of examples.
    a new program (six_degrees) prints shortest connections between
    actors.

---
+++ sandbox/SOC/2007/graphs/libs/graph/examples/kevin_bacon.cpp
@@ -0,0 +1,88 @@
+// (C) Copyright Andrew Sutton 2007
+//
+// Use, modification and distribution are subject to the
+// Boost Software License, Version 1.0 (See accompanying file
+// LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+
+#include <iostream>
+
+#include <boost/graph/visitors.hpp>
+#include <boost/graph/breadth_first_search.hpp>
+
+#include "movies.hpp"
+
+using namespace std;
+using namespace boost;
+
+// This visitor is responsible for recording (setting) the bacon numbers
+// of actors in the movie graph.
+template <typename BaconMap>
+struct BaconNumberRecorder : public bfs_visitor<>
+{
+    BaconNumberRecorder(BaconMap b)
+	: bacons(b)
+    {}
+
+    // The tree_edge() method is invoked when a vertex v is discovered
+    // while exploring an edge (u,v). With respect to a BFS, this means
+    // that v is in the next higher level (or depth) than u. Therefore,
+    // the Kevin Bacon number of v should be one more than that of u.
+    template <typename Edge, typename Graph>
+    void tree_edge(Edge e, const Graph& g) const
+    {
+	typedef typename Graph::vertex_descriptor Vertex;
+
+	// get the source and target vertices of this tree edge.
+	Vertex
+	    u = source(e, g),
+	    v = target(e, g);
+
+	// set the bacon number to 1 + u's bacon number
+	bacons[v] = bacons[u] + 1;
+    }
+
+    BaconMap bacons;
+};
+
+// This is just a convenience function so we can call a function rather than
+// explicitly instantiate the visitor type. It makes it more "action-oriented".
+template <typename BaconMap>
+BaconNumberRecorder<BaconMap> record_bacons(BaconMap b)
+{
+    return BaconNumberRecorder<BaconMap>(b);
+}
+
+int
+main(int argc, char *argv[])
+{
+    // instantiate the graph
+    Graph g;
+
+    // instantiate the graph-construction helping map
+    ActorMap actors;
+
+    // read the graph from stdin
+    build_movie_graph(cin, g, actors);
+
+    // get the bacon number map associated with the graph
+    ActorNameMap names = get(&Actor::name, g);
+    ActorBaconMap bacons = get(&Actor::bacon_number, g);
+
+    // pick a starting vertex (kevin bacon, obviously) and set his
+    // number to 0.
+    Vertex kevin = actors["Kevin Bacon"];
+    bacons[kevin] = 0;
+
+    // run a breadth-first search on the graph and record
+    // the kevin bacon numbers for each actor
+    breadth_first_search(g, kevin, visitor(record_bacons(bacons)));
+
+    // just run over the vertices and print the related info
+    Graph::vertex_iterator i, j;
+    for(tie(i, j) = vertices(g); i != j; ++i) {
+	cout << bacons[*i] << " : " << names[*i] << "\n";
+    }
+
+    return 0;
+}
+
