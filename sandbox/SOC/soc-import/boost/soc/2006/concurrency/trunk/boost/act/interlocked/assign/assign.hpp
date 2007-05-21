e36ec69013c565bdd739c65d08dbb38fd2f3de10
Matthew Calabrese <rivorus@gmail.com>
2007-05-21 18:32:07 +0000

    ** empty log message **

---
+++ sandbox/SOC/soc-import/boost/soc/2006/concurrency/trunk/boost/act/interlocked/assign/assign.hpp
@@ -0,0 +1,33 @@
+/*=============================================================================
+    Copyright (c) 2006, 2007 Matthew Calabrese
+
+    Use, modification and distribution is subject to the Boost Software
+    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef BOOST_ACT_INTERLOCKED_ASSIGN_ASSIGN_HPP
+#define BOOST_ACT_INTERLOCKED_ASSIGN_ASSIGN_HPP
+
+#include <boost/act/interlocked/assign/detail/assign_impl.hpp>
+
+#include <boost/utility/enable_if.hpp>
+#include <boost/type_traits/remove_volatile.hpp>
+
+namespace boost { namespace act { namespace interlocked {
+
+template< typename TargetType, typename SourceType >
+typename lazy_enable_if
+<
+  detail::assign::are_valid_params< TargetType, SourceType >
+, remove_volatile< TargetType >
+>
+::type
+assign( TargetType& destination, SourceType const& new_value )
+{
+  return detail::assign::assign( destination, new_value );
+}
+
+} } }
+
+#endif
