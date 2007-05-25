1e95b631bc36d93bfd3dbd192bc2a28e44429435
Jeffrey Faust <jeff@opticalres.com>
2007-05-25 00:56:34 +0000

    Add user-defined test.

---
+++ sandbox/explore/libs/explore/test/user_defined_container.cpp
@@ -0,0 +1,64 @@
+// Boost.Print library
+
+// Copyright Jared McIntyre 2007. Use, modification and
+// distribution is subject to the Boost Software License, Version
+// 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// For more information, see http://www.boost.org
+
+#define BOOST_TEST_MODULE PrintLib
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+#include <vector>
+#include "../../../boost/explore/explore.hpp"
+#include "../../../boost/explore/stream_container.hpp"
+
+class user_vector
+{
+public:
+    user_vector()
+    {
+        m_vec.push_back(1);
+        m_vec.push_back(2);
+        m_vec.push_back(3);
+    }
+
+    std::vector<int>::const_iterator begin() const
+    {
+        return m_vec.begin();
+    }
+
+    std::vector<int>::const_iterator end() const
+    {
+        return m_vec.end();
+    }
+
+private:
+    std::vector<int> m_vec;
+};
+
+std::ostream& operator<<(std::ostream& ostr, user_vector& u)
+{
+    return boost::stream_container(ostr, u.begin(), u.end());
+}
+
+BOOST_AUTO_TEST_CASE( user_defined_print_test )
+{
+    std::stringstream str_out;
+
+    user_vector v;
+    explore::print(v, str_out);
+    BOOST_CHECK_EQUAL(str_out.str(), "[1, 2, 3]");
+}
+
+BOOST_AUTO_TEST_CASE( user_defined_stream_test )
+{
+    using namespace boost;
+    std::stringstream str_out;
+
+    user_vector v;
+    str_out << v;
+    BOOST_CHECK_EQUAL(str_out.str(), "[1, 2, 3]");
+}
+
