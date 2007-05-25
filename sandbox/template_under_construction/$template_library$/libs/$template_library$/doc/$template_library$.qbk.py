b73d266cd7fd07ca06bf75758965f39330599671
Stjepan Rajko <stipe@asu.edu>
2007-05-25 18:02:50 +0000

    FileTemplate version 0.9

---
+++ sandbox/template_under_construction/$template_library$/libs/$template_library$/doc/$template_library$.qbk.py
@@ -0,0 +1,73 @@
+# template file
+
+# Copyright 2007 Stjepan Rajko.
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+docs = template.options.get('docs')
+                     
+content="""
+[library $template_Library$
+    [quickbook 1.4]
+    [version 0.01e-10]
+    [authors $template_author_reversed$]
+    [copyright $template_year$ $template_author$]
+    [purpose Doing nothing.]
+    [license
+        Distributed under the Boost Software License, Version 1.0.
+        (See accompanying file LICENSE_1_0.txt or copy at
+        [@http://www.boost.org/LICENSE_1_0.txt])
+    ]
+]
+
+Welcome to the $template_Library$!  One day, it will perhaps be called
+$template_Boost_Library$. Until then, feel free to use it to do nothing.
+
+[warning $template_Library$ is not an official Boost library.]
+
+[section:introduction Introduction]
+
+User, this is $template_Library$.  $template_Library$, this is the user.  Be nice to each other.
+
+[section:start Getting Started]
+
+The $template_Library$ is located in the $template_library$ directory of the boost sandbox.
+
+"""
+if docs!='qb':
+    content+="""
+You might also want to look at the [link $template_library$.reference reference].
+
+"""
+
+content+="""
+[endsect]
+
+[endsect]
+"""
+if docs=='qb+doxy':
+    content+="""
+[section:reference Reference]
+
+The reference for this library is [@doxygen/index.html generated separately].
+
+[endsect]
+
+"""
+
+content+="""
+[section:license License]
+
+$template_qbk_copyright$
+
+[endsect]
+"""
+
+if docs=='qb+doxyref':
+    content+="""
+[xinclude $template_library$_doxygen.xml]
+"""
+
+template.submit_content(content)
+
--- sandbox/template_under_construction/$template_library$/libs/$template_library$/doc/Jamfile.v2.py
+++ sandbox/template_under_construction/$template_library$/libs/$template_library$/doc/Jamfile.v2.py
@@ -1,9 +1,15 @@
+# template file
+
 # Copyright 2007 Stjepan Rajko.
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
-$template_start$
+docs = template.options.get('docs')
+doxygen = docs != 'qb'
+    
+
+content = """$template_start$
 $template_python_copyright$
 
 project boost/$template_library$/doc ;
@@ -14,6 +20,10 @@ using quickbook ;
 local loc = [ path.native [ path.pwd ] ] ;
 local root = [ path.native [ path.join [ path.pwd ] ../../.. ] ] ;
 
+xml $template_library$_xml : $template_library$.qbk ;"""
+
+if doxygen:
+    content += """
 doxygen $template_library$_doxygen
    :
       [ glob ../../../boost/$template_library$/*.hpp dox/*.hpp ]
@@ -25,20 +35,37 @@ doxygen $template_library$_doxygen
         <doxygen:param>HIDE_UNDOC_MEMBERS=NO
         <doxygen:param>EXTRACT_PRIVATE=NO
         <doxygen:param>DETAILS_AT_TOP=YES
-        <doxygen:param>GENERATE_HTML=YES
-        <doxygen:param>HTML_OUTPUT=$(loc)/html/doxygen
-        <doxygen:param>HTML_STYLESHEET=$(loc)/html/boostbook_doxygen.css
         <doxygen:param>ENABLE_PREPROCESSING=YES
         <doxygen:param>MACRO_EXPANSION=YES
         <doxygen:param>SEARCH_INCLUDES=YES
         <doxygen:param>INCLUDE_PATH=../../..
-        <doxygen:param>PREDEFINED=DOXYGEN_DOCS_ONLY
+        <doxygen:param>PREDEFINED=DOXYGEN_DOCS_ONLY"""
+
+    if docs=='qb+doxy':
+        content += """
+        <doxygen:param>GENERATE_HTML=YES
+        <doxygen:param>HTML_OUTPUT=$(loc)/html/doxygen
+        <doxygen:param>HTML_STYLESHEET=$(loc)/html/boostbook_doxygen.css"""
+    content +="""
    ;
+"""
 
-xml $template_library$_xml : $template_library$.qbk ;
-
+content += """
 boostbook standalone
    :
-      $template_library$_xml
-      $template_library$_doxygen
-   ;
+      $template_library$_xml"""
+if (doxygen):
+    content+="""
+      $template_library$_doxygen"""
+
+content +="""
+    :
+        <xsl:param>chunk.first.sections=1
+        <xsl:param>chunk.section.depth=3
+        <xsl:param>toc.section.depth=3
+        <xsl:param>toc.max.depth=3
+        <xsl:param>generate.section.toc.level=3
+    ;
+"""
+
+template.submit_content(content)
