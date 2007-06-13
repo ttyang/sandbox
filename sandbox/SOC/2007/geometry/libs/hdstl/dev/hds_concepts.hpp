2ead04abafa85ee84811153b68ce171e00e36c77
Huseyin Akcan <huseyinakcan@gmail.com>
2007-06-13 20:47:14 +0000

    *_list_hds_concept concept definitions and classes added.

--- sandbox/SOC/2007/geometry/libs/hdstl/dev/hds_concepts.hpp
+++ sandbox/SOC/2007/geometry/libs/hdstl/dev/hds_concepts.hpp
@@ -1,18 +1,18 @@
+//hds_concepts.hpp   -*- C++ -*-
+//
+//@PURPOSE: Provides concept checking classes for HDS-related concepts.
+//
+///HDS Introduction
+///----------------
+// FILLME
+//
+///HDS Concepts
+///------------
+// FILLME
+
 #ifndef HDSTL_HDS_CONCEPTS_HPP
 #define HDSTL_HDS_CONCEPTS_HPP
 
-/**
- * @file hds_concepts.hpp
- *
- * @brief This file provides a number of concept checking classes for
- * HDS-related concepts.
- *
- * @par HDS Introduction
- * FILLME
- *
- * @par HDS Concepts
- * FILLME
- */
 #include <hds_concepts/hds_concept.hpp>
 #include <hds_concepts/forward_hds_concept.hpp>
 #include <hds_concepts/backward_hds_concept.hpp>
@@ -20,4 +20,5 @@
 #include <hds_concepts/vertex_hds_concept.hpp>
 #include <hds_concepts/facet_hds_concept.hpp>
 #include <hds_concepts/mutable_hds_concept.hpp>
+
 #endif
