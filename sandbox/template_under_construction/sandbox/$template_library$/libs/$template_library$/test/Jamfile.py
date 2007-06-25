d882794e9f03379da3f366dab84cb275aba2347a
Stjepan Rajko <stipe@asu.edu>
2007-06-25 17:41:41 +0000

    change make_template to file_template, author to authors, and other proposed changes, start Xcode IDE generation support

--- sandbox/template_under_construction/sandbox/$template_library$/libs/$template_library$/test/Jamfile.py
+++ sandbox/template_under_construction/sandbox/$template_library$/libs/$template_library$/test/Jamfile.py
@@ -5,7 +5,7 @@
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
-template.append_content("""$template_python_copyright$
+template.append_content("""$template_copyright$
 
 import testing ;
 
