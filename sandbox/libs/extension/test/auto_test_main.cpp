a378fbd94e027b902fd9c37ec9c6c167e0ccb545
Mariano Gabriel Consoni <mariano.consoni@gmail.com>
2007-06-27 15:37:37 +0000

    - added license info and copyright notices.

--- sandbox/libs/extension/test/auto_test_main.cpp
+++ sandbox/libs/extension/test/auto_test_main.cpp
@@ -1,12 +1,13 @@
-/*******************************************************
- * @file auto_test_main.cpp
- * @brief 
- * @author Vladimir Volod'ko
- * @date 2007/03/23
- *******************************************************/
-
-/// This file is used for ability to build all test case files in one executable.
-/// One may link any test case file with this file to build separate test case executable.
+/*
+ * Boost.Extension / this file is used to build all test cas files in one executable
+ *
+ * (C) Copyright Jeremy Pack 2007
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * See http://www.boost.org/ for latest version.
+ */
 
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
