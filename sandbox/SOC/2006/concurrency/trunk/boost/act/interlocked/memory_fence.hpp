92b86fa144cec01fd9a412165601b5e0e2fd8f5a
Matthew Calabrese <rivorus@gmail.com>
2007-07-14 08:46:15 +0000

    Doc updates, added some more support for the additional bidirectional semantics. GCC x86 support is broken for this release temporarily.

--- sandbox/SOC/2006/concurrency/trunk/boost/act/interlocked/memory_fence.hpp
+++ sandbox/SOC/2006/concurrency/trunk/boost/act/interlocked/memory_fence.hpp
@@ -6,15 +6,15 @@
     http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 
-#ifndef BOOST_ACT_INTERLOCKED_FENCE_HPP
-#define BOOST_ACT_INTERLOCKED_FENCE_HPP
+#ifndef BOOST_ACT_INTERLOCKED_MEMORY_FENCE_HPP
+#define BOOST_ACT_INTERLOCKED_MEMORY_FENCE_HPP
 
-#include <boost/act/interlocked/fence/fence.hpp>
-#include <boost/act/interlocked/fence/fence_thread_unsafe.hpp>
-#include <boost/act/interlocked/fence/fence_unordered.hpp>
-#include <boost/act/interlocked/fence/fence_acquire.hpp>
-#include <boost/act/interlocked/fence/fence_release.hpp>
-#include <boost/act/interlocked/fence/fence_acq_rel.hpp>
-#include <boost/act/interlocked/fence/fence_sequential.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_thread_unsafe.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_unordered.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_acquire.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_release.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_acq_rel.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_sequential.hpp>
 
 #endif
--- sandbox/SOC/2006/concurrency/trunk/boost/act/interlocked/memory_fence_fwd.hpp
+++ sandbox/SOC/2006/concurrency/trunk/boost/act/interlocked/memory_fence_fwd.hpp
@@ -6,15 +6,15 @@
     http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 
-#ifndef BOOST_ACT_INTERLOCKED_FENCE_FWD_HPP
-#define BOOST_ACT_INTERLOCKED_FENCE_FWD_HPP
+#ifndef BOOST_ACT_INTERLOCKED_MEMORY_FENCE_FWD_HPP
+#define BOOST_ACT_INTERLOCKED_MEMORY_FENCE_FWD_HPP
 
-#include <boost/act/interlocked/fence/fence_fwd.hpp>
-#include <boost/act/interlocked/fence/fence_thread_unsafe_fwd.hpp>
-#include <boost/act/interlocked/fence/fence_unordered_fwd.hpp>
-#include <boost/act/interlocked/fence/fence_acquire_fwd.hpp>
-#include <boost/act/interlocked/fence/fence_release_fwd.hpp>
-#include <boost/act/interlocked/fence/fence_acq_rel_fwd.hpp>
-#include <boost/act/interlocked/fence/fence_sequential_fwd.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_fwd.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_thread_unsafe_fwd.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_unordered_fwd.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_acquire_fwd.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_release_fwd.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_acq_rel_fwd.hpp>
+#include <boost/act/interlocked/memory_fence/memory_fence_sequential_fwd.hpp>
 
 #endif
