4bfe137ad96c9ae9d37f66563246b3afaa8307c9
Andrew Sutton <andrew.n.sutton@gmail.com>
2007-08-01 13:49:13 +0000

    Moved distance tests to closeness centrality Added mean geodesic tests

--- sandbox/SOC/2007/graphs/libs/graph/test/closeness_centrality.cpp
+++ sandbox/SOC/2007/graphs/libs/graph/test/closeness_centrality.cpp
@@ -10,13 +10,27 @@
 #include <vector>
 #include <tr1/unordered_map>
 
+#include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/undirected_graph.hpp>
 #include <boost/graph/directed_graph.hpp>
 #include <boost/graph/exterior_property.hpp>
-#include <boost/graph/degree_centrality.hpp>
+
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/graph/floyd_warshall_shortest.hpp>
+#include <boost/graph/closeness_centrality.hpp>
 
 using namespace std;
 using namespace boost;
+
+struct VertexProp
+{
+    int dummy;
+};
+
+struct EdgeProp
+{
+    int weight;
+};
 
 template <typename Graph>
 void build_graph(Graph& g)
@@ -35,11 +49,17 @@ void build_graph(Graph& g)
     }
 
     // add some edges (with weights)
-    add_edge(v[0], v[1], g);
-    add_edge(v[1], v[2], g);
-    add_edge(v[2], v[0], g);
-    add_edge(v[3], v[4], g);
-    add_edge(v[4], v[0], g);
+    e.push_back(add_edge(v[0], v[1], g).first);
+    e.push_back(add_edge(v[1], v[2], g).first);
+    e.push_back(add_edge(v[2], v[0], g).first);
+    e.push_back(add_edge(v[3], v[4], g).first);
+    e.push_back(add_edge(v[4], v[0], g).first);
+
+    g[e[0]].weight = 1;
+    g[e[1]].weight = 1;
+    g[e[2]].weight = 1;
+    g[e[3]].weight = 1;
+    g[e[4]].weight = 1;
 };
 
 template <typename Graph, typename PropertyMap>
@@ -53,31 +73,53 @@ void print_map(const Graph& g, PropertyMap pm)
     cout << "}\n";
 }
 
+template <typename Graph, typename Matrix>
+void print_matrix(const Graph& g, Matrix m)
+{
+    cout << "[\n";
+    typename Graph::vertex_iterator i, j, end;
+    for(tie(i, end) = vertices(g); i != end; ++i) {
+        print_map(g, m[*i]);
+    }
+    cout << "]\n";
+}
+
 template <typename Graph>
 void test()
 {
     typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
     typedef typename graph_traits<Graph>::edge_descriptor Edge;
 
-    typedef exterior_vertex_property<Graph, unsigned> CentralityProperty;
+    typedef exterior_vertex_property<Graph, float> CentralityProperty;
     typedef typename CentralityProperty::container_type CentralityContainer;
     typedef typename CentralityProperty::map_type CentralityMap;
+
+    typedef exterior_vertex_property<Graph, int> DistanceProperty;
+    typedef typename DistanceProperty::matrix_type DistanceMatrix;
+
+    typedef typename property_map<Graph, int EdgeProp::*>::type WeightMap;
 
     Graph g;
     build_graph(g);
 
     CentralityContainer centralities(num_vertices(g));
-    CentralityMap cents(centralities);
-    degree_centrality(g, cents);
+    CentralityMap cent(centralities);
+    DistanceMatrix dist(num_vertices(g));
+    WeightMap weights(get(&EdgeProp::weight, g));
 
-    print_map(g, cents);
+    floyd_warshall_all_pairs_shortest_paths(g, dist, weight_map(weights));
+    closeness_centrality(g, dist, cent);
+
+    print_matrix(g, dist);
+    print_map(g, cent);
+
 }
 
 int
 main(int argc, char *argv[])
 {
-    typedef undirected_graph<> Graph;
-    typedef directed_graph<> Digraph;
+    typedef undirected_graph<VertexProp, EdgeProp> Graph;
+    typedef directed_graph<VertexProp, EdgeProp> Digraph;
 
     cout << "\n*** undirected_graph<> *** \n";
     test<Graph>();
