f4a615fbda65a4c749c5379d7e81254b1fbb239d
Andrew Sutton <andrew.n.sutton@gmail.com>
2008-06-03 00:17:21 +0000

    Work on retagging impl.

---
+++ sandbox/SOC/2008/graphs/branches/tagged/boost/graphs/adjacency_list/undirected/undirected.hpp
@@ -0,0 +1,18 @@
+
+#ifndef BOOST_GRAPHS_ADJACENCY_LIST_UNDIRECTED_HPP
+#define BOOST_GRAPHS_ADJACENCY_LIST_UNDIRECTED_HPP
+
+namespace boost {
+namespace graphs {
+namespace adj_list {
+
+struct undirected { };
+
+} /* namespace adj_list */
+} /* namespace graphs */
+} /* namespace boost */
+
+#include <boost/graphs/adjacency_list/undirected/vertex.hpp>
+// #include <boost/graphs/adjacency_list/undirected/edge.hpp>
+
+#endif
