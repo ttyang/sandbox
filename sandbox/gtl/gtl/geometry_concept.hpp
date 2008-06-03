d81baec1768403c821305f793cd9f5f8f78b6f2c
Luke Simonson <simoluk@gmail.com>
2008-06-03 23:56:56 +0000

    adding 45 and aribtrary angle polygons, fleshing out API for basic types

---
+++ sandbox/gtl/gtl/geometry_concept.hpp
@@ -0,0 +1,75 @@
+/*
+  Copyright 2008 Intel Corporation
+ 
+  Use, modification and distribution are subject to the Boost Software License,
+  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+  http://www.boost.org/LICENSE_1_0.txt).
+*/
+
+template <typename T>
+struct geometry_concept { 
+  typedef no_type type;
+};
+
+#define GTL_REGISTER_GEOMETRY_TRAITS(coordinate_type) \
+template <>\
+struct geometry_concept<point_data<coordinate_type> > {\
+  typedef point_concept type;\
+};\
+template <>\
+struct geometry_concept<rectangle_data<coordinate_type> > {\
+  typedef rectangle_concept type;\
+};\
+template <>\
+struct geometry_concept<interval_data<coordinate_type> > {\
+  typedef interval_concept type;\
+};\
+template <>\
+struct geometry_concept<point_3d_data<coordinate_type> > {\
+  typedef point_3d_concept type;\
+};\
+template <>\
+struct geometry_concept<polygon_90_data<coordinate_type> > {\
+  typedef polygon_90_concept type;\
+};\
+template <>\
+struct geometry_concept<polygon_90_with_holes_data<coordinate_type> > {\
+  typedef polygon_90_with_holes_concept type;\
+};\
+template<>\
+struct geometry_concept<polygon_formation::PolyLinePolygonData<true, coordinate_type> > {\
+  typedef polygon_90_with_holes_concept type;\
+};\
+template<>\
+struct geometry_concept<polygon_formation::PolyLinePolygonData<false, coordinate_type> > {\
+  typedef polygon_90_with_holes_concept type;\
+};\
+template <>\
+struct geometry_concept<polygon_formation::PolyLineHoleData<true, coordinate_type> > {\
+  typedef polygon_90_concept type;\
+};\
+template <>\
+struct geometry_concept<polygon_formation::PolyLineHoleData<false, coordinate_type> > {\
+  typedef polygon_90_concept type;\
+};\
+template <>\
+struct geometry_concept<polygon_45_data<coordinate_type> > {\
+  typedef polygon_45_concept type;\
+};\
+template <>\
+struct geometry_concept<polygon_45_with_holes_data<coordinate_type> > {\
+  typedef polygon_45_with_holes_concept type;\
+};\
+template <>\
+struct geometry_concept<polygon_data<coordinate_type> > {\
+  typedef polygon_concept type;\
+};\
+template <>\
+struct geometry_concept<polygon_with_holes_data<coordinate_type> > {\
+  typedef polygon_with_holes_concept type;\
+};\
+
+//end of GTL_REGISTER_GEOMETRY_TRAITS macro
+
+GTL_REGISTER_GEOMETRY_TRAITS(int)
+GTL_REGISTER_GEOMETRY_TRAITS(long long)
