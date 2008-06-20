f8817bde1acd5123b8b6b4d1d7ed7be24bc9bf4f
Andrew Sutton <andrew.n.sutton@gmail.com>
2008-06-20 13:28:59 +0000

    Finished initial implementation of edge sets.

--- sandbox/SOC/2008/graphs/trunk/boost/graphs/edge_set.hpp
+++ sandbox/SOC/2008/graphs/trunk/boost/graphs/edge_set.hpp
@@ -4,38 +4,65 @@
 
 #include "property_list.hpp"
 #include "incidence_set.hpp"
+#include "out_set.hpp"
+#include "in_set.hpp"
 
 /**
  * The edge set metafunction defines the basic facets of set-based edge
  * storage. Edge sets are best used to implement simple graphs (that do not
  * allow multiple edges).
  *
- * There are three parmaters to this function:
- * Compare - A function that compares vertex descriptors
- * IncAlloc - An allocator for the edge set
- * PropAlloc - An allocator for the global property set.
- *
- * Note that the edge set uses a map for the incidence store.
+ * @param Compare - A unary template class that compares vertex descriptors.
+ * @param FirstAlloc - An allocator template for either incident edges or out edges.
+ * @param SecondAlloc - An allocator template for either global properties or in edges.
  */
 template <
     template <typename> class Compare,
-    template <typename> class IncAlloc,
-    template <typename> class PropAlloc>
+    template <typename> class FirstAlloc,
+    template <typename> class SecondAlloc>
 struct basic_edge_set
 {
+    // The property store metafunnction generates the global store type
+    // for undirected graphs.
     template <typename EdgeProps>
     struct property_store
     {
-        typedef property_list<EdgeProps, PropAlloc<EdgeProps> > type;
+        typedef SecondAlloc<EdgeProps> allocator;
+        typedef property_list<EdgeProps, allocator> type;
     };
 
+    // The incidence store metafunction generates the per-vertex stores for
+    // incident edges.
     template <typename VertexDesc, typename PropDesc>
     struct incidence_store
     {
         typedef std::pair<VertexDesc, PropDesc> value;
         typedef Compare<VertexDesc> compare;
-        typedef IncAlloc<value> alloc;
-        typedef incidence_set<value, compare, alloc> type;
+        typedef FirstAlloc<value> allocator;
+        typedef incidence_set<value, compare, allocator> type;
+    };
+
+    // The out store metafunction generates the type of set used to store out
+    // edges of a vertex in a directed graph.
+    template <typename VertexDesc, typename Props>
+    struct out_store
+    {
+        typedef std::pair<VertexDesc, Props> out_pair;
+        typedef Compare<VertexDesc> compare;
+        typedef FirstAlloc<out_pair> allocator;
+        typedef out_set<out_pair, compare, allocator> type;
+    };
+
+    // The in store metafunction generates the type of list used to store
+    // incoming edges of directed graph. In edges are represented by the
+    // referencing vertex and an out edge descriptor.
+    template <typename VertexDesc, typename OutDesc>
+    struct in_store
+    {
+        typedef std::pair<VertexDesc, OutDesc> in_pair;
+        typedef Compare<VertexDesc> compare;
+        typedef SecondAlloc<in_pair> allocator;
+        typedef in_set<in_pair, compare, allocator> type;
     };
 };
 
