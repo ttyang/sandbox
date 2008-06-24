65d7217b11e13e531374bcb513f7a4b8edf44b6f
Andrew Sutton <andrew.n.sutton@gmail.com>
2008-06-24 15:10:08 +0000

    Cleaned up more code. Got the impl of remove edges the way I wanted it to work for both directed/undirected.

--- sandbox/SOC/2008/graphs/trunk/boost/graphs/incidence_list.hpp
+++ sandbox/SOC/2008/graphs/trunk/boost/graphs/incidence_list.hpp
@@ -23,167 +23,76 @@ public:
     typedef typename IncEdge::second_type property_descriptor;
 
     typedef typename store_type::iterator iterator;
-    typedef typename store_type::const_iterator const_iterator;
     typedef typename store_type::size_type size_type;
 
     // Constructors
-    incidence_list();
+    incidence_list()
+        : _edges()
+        , _size(0)
+    { }
 
-    inline void add(incidence_pair);
+    /**
+     * Incidence lists always allow the addition of edges, assuming that no policy
+     * conflicts exist. The first element of the return is the end() of the list.
+     * @complexity O(1)
+     */
+    inline std::pair<iterator, bool> allow(vertex_descriptor) const
+    { return make_pair(_edges.end(), true); }
 
-    inline std::pair<const_iterator, bool> allow(vertex_descriptor) const;
-    inline iterator find(incidence_pair);
-    inline const_iterator find(incidence_pair) const;
+    /**
+     * Add a vertex to the list.
+     * @complexity O(1)
+     */
+    inline void add(incidence_pair p)
+    {
+        ++_size;
+        _edges.push_back(p);
+    }
 
-    inline void clear();
-    inline void remove(incidence_pair);
-    template <typename Eraser> inline void remove(vertex_descriptor, Eraser);
+    /**
+     * Find the given incidence pair in the vertex.
+     * @todo Do we need this function?
+     * @complexity O(1)
+     */
+    inline iterator find(incidence_pair p) const
+    { return std::find(_edges.begin(), _edges.end(), p); }
+
+    /**
+     * Remove the given incidence pair in this vertex.
+     * @complexity O(deg(v))
+     */
+    inline void remove(incidence_pair p)
+    { remove(find(p)); }
+
+    /**
+     * Remove the iterator.
+     * @complexity O(1)
+     */
+    inline iterator remove(iterator i)
+    {
+        --_size;
+        return _edges.erase(i);
+    }
+
+    /** Remove all edges from the vertex. */
+    inline void clear()
+    {
+        _size = 0;
+        _edges.clear();
+    }
 
     inline size_type size() const
-    { return _edges.size(); }
+    { return _size; }
 
-    inline const_iterator begin() const
+    inline iterator begin() const
     { return _edges.begin(); }
 
-    inline const_iterator end() const
+    inline iterator end() const
     { return _edges.end(); }
 
 private:
-    store_type _edges;
+    mutable store_type  _edges;
+    size_type           _size;
 };
-
-#define BOOST_GRAPH_IL_PARAMS \
-    typename E, typename A
-
-template <BOOST_GRAPH_IL_PARAMS>
-incidence_list<E,A>::incidence_list()
-    : _edges()
-{ }
-
-/**
- * Incidence lists always allow the addition of edges, assuming that no policy
- * conflicts exist. The first element of the return is the end() of the list.
- *
- * @complexity O(1)
- */
-template <BOOST_GRAPH_IL_PARAMS>
-std::pair<typename incidence_list<E,A>::const_iterator, bool>
-incidence_list<E,A>::allow(vertex_descriptor v) const
-{
-    // Since there's no policy, there we must be able to add the edge.
-    return make_pair(_edges.end(), true);
-}
-
-/**
- * Add the incident edge to the incidence set.
- *
- * @complexity O(1)
- */
-template <BOOST_GRAPH_IL_PARAMS>
-void
-incidence_list<E,A>::add(incidence_pair p)
-{
-    _edges.push_back(p);
-}
-
-/**
- * Remove all incident edges from this set.
- *
- * @complexity  O(d)
- */
-template <typename E, typename A>
-void
-incidence_list<E,A>::clear()
-{
-    _edges.clear();
-}
-
-/**
- * Remove the incidence pair from the list. This operation is linear with
- * respect to the number of incident edges.
- *
- * @require EqualityComparable<vertex_descriptor>
- * @require EqualityComparable<property_descriptor>
- * @complexity O(d)
- */
-template <BOOST_GRAPH_IL_PARAMS>
-void
-incidence_list<E,A>::remove(incidence_pair p)
-{
-    // Find the pair in the list and erase it.
-    iterator iter = std::find(_edges.begin(), _edges.end(), p);
-    this->_edges.erase(iter);
-}
-
-/**
- * Remove the all edges connecting the adjacent vertex from the list. This
- * operations is exactly linear w.r.t. the number of incident edges.
- *
- * @require EqualityComparable<vertex_descriptor>
- * @require EqualityComparable<property_descriptor>
- * @complexity Theta(d)
- */
-template <BOOST_GRAPH_IL_PARAMS>
-template <typename Eraser>
-void
-incidence_list<E,A>::remove(vertex_descriptor v, Eraser erase)
-{
-    iterator i = _edges.begin(), end = _edges.end();
-    for( ; i != end; ) {
-        if(i->first == v) {
-            erase(i->second);
-            i = _edges.erase(i);
-        }
-        else {
-            ++i;
-        }
-    }
-}
-
-#if 0
-
-// Functions
-
-template <typename E>
-vertex_edge_list<E>::vertex_edge_list()
-    : base_type()
-{ }
-
-template <typename E>
-void
-vertex_edge_list<E>::add(edge_descriptor e)
-{
-    this->_store.push_back(e);
-}
-
-template <typename E>
-typename vertex_edge_list<E>::iterator
-vertex_edge_list<E>::find(edge_descriptor e)
-{
-    return std::find(this->_store.begin(), this->_store.end(), e);
-}
-
-template <typename E>
-typename vertex_edge_list<E>::const_iterator
-vertex_edge_list<E>::find(edge_descriptor e) const
-{
-    return std::find(this->_store.begin(), this->_store.end(), e);
-}
-
-template <typename E>
-void
-vertex_edge_list<E>::clear()
-{
-    this->_store.clear();
-}
-
-template <typename E>
-typename vertex_edge_list<E>::size_type
-vertex_edge_list<E>::size() const
-{
-    return this->_store.size();
-}
-
-#endif
 
 #endif
--- sandbox/SOC/2008/graphs/trunk/boost/graphs/incidence_set.hpp
+++ sandbox/SOC/2008/graphs/trunk/boost/graphs/incidence_set.hpp
@@ -4,6 +4,8 @@
 
 #include <map>
 
+#include <boost/next_prior.hpp>
+
 /**
  * The incidence vector stores incident "edges" of a vertex. In actuality,
  * this stores pairs containing an adjacent vertex descriptor and a property
@@ -27,161 +29,71 @@ private:
     typedef std::map<vertex_descriptor, property_descriptor, Compare, Alloc> store_type;
 public:
     typedef typename store_type::iterator iterator;
-    typedef typename store_type::const_iterator const_iterator;
     typedef typename store_type::size_type size_type;
 
     // Constructors
-    incidence_set();
+    inline incidence_set()
+        : _edges()
+    { }
 
-    // Add an incident edge.
-    inline void add(incidence_pair);
+    /**
+     * Deteremine whether or not the edge exists or is even allowed to be added.
+     * This returns a pair containing an iterator indicating the position of the
+     * edge if it already exists and a bool value indicating whether or not the
+     * addition would even be allowed by policy.
+     * @complexity O(lg(deg(v)))
+     */
+    inline std::pair<iterator, bool> allow(vertex_descriptor v) const
+    { return make_pair(_edges.find(v), true); }
 
-    // Allow a connection to the edge?
-    inline std::pair<const_iterator, bool> allow(vertex_descriptor) const;
+    /**
+     * Add the incidence pair to the vertex.
+     * @complexity O(lg(deg(v)))
+     */
+    inline void add(incidence_pair p)
+    { _edges.insert(p); }
 
-    // Find the edge record in question.
-    inline iterator find(incidence_pair);
-    inline const_iterator find(incidence_pair) const;
+    /**
+     * Find the incidence pair.
+     * @complexity O(lg(deg(v)))
+     */
+    inline iterator find(incidence_pair p) const
+    { return _edges.find(p.first); }
+
+    /**
+     * Remove the incidence pair from the set.
+     * @complexity O(lg(deg(v)))
+     */
+    inline void remove(incidence_pair p)
+    { _edges.erase(find(p)); }
+
+    /**
+     * Remove the iterator to the given incidence pair, returning an iterator
+     * to the next object in the sequence.
+     * @complexity O(lg(deg(v)))
+     */
+    inline iterator remove(iterator x)
+    {
+        iterator ret = boost::next(x);
+        _edges.erase(x);
+        return ret;
+    }
 
     // Remove edges.
-    inline void clear();
-    inline void remove(incidence_pair);
-    template <typename Eraser> inline void remove(vertex_descriptor, Eraser);
-
+    inline void clear()
+    { _edges.clear(); }
 
     inline size_type size() const
     { return _edges.size(); }
 
-    inline const_iterator begin() const
+    inline iterator begin() const
     { return _edges.begin(); }
 
-    inline const_iterator end() const
+    inline iterator end() const
     { return _edges.end(); }
 
 private:
-    store_type _edges;
+    mutable store_type _edges;
 };
-
-// Functions
-
-#define BOOST_GRAPH_IS_PARAMS \
-    typename E, typename C, typename A
-
-template <BOOST_GRAPH_IS_PARAMS>
-incidence_set<E,C,A>::incidence_set()
-    : _edges()
-{ }
-
-/**
- * Deteremine whether or not the edge exists or is even allowed to be added.
- * This returns a pair containing an iterator indicating the position of the
- * edge if it already exists and a bool value indicating whether or not the
- * addition would even be allowed by policy.
- *
- * @complexity O(lg(d))
- */
-template <BOOST_GRAPH_IS_PARAMS>
-std::pair<typename incidence_set<E,C,A>::const_iterator, bool>
-incidence_set<E,C,A>::allow(vertex_descriptor v) const
-{
-    // For now, always assume that the edge is allowed by policy, and determine
-    // "addability" based on whehter or not the edge exists.
-    return make_pair(_edges.find(v), true);
-}
-
-
-/**
- * Add the incidence pair to the set.
- *
- * @complexity O(lg(d))
- */
-template <BOOST_GRAPH_IS_PARAMS>
-void
-incidence_set<E,C,A>::add(incidence_pair p)
-{
-    _edges.insert(p);
-}
-
-/**
- * Remove all incident edges from this edge set.
- */
-template <typename E, typename C, typename A>
-void
-incidence_set<E,C,A>::clear()
-{
-    _edges.clear();
-}
-
-/**
- * Remove the incidence pair from the set.
- *
- * @complexity O(lg(d))
- */
-template <BOOST_GRAPH_IS_PARAMS>
-void
-incidence_set<E,C,A>::remove(incidence_pair p)
-{
-    // The erase function takes a key! Not the entire pair.
-    _edges.erase(p.first);
-}
-
-/**
- * Remove the incident edge indicated by the given vertex and invoke the
- * erase function on the associated property descriptor.
- */
-template <BOOST_GRAPH_IS_PARAMS>
-template <typename Erase>
-void
-incidence_set<E,C,A>::remove(vertex_descriptor v, Erase erase)
-{
-    // Find the mapped property descriptor before erasing the edge.
-    iterator iter = _edges.find(v);
-    property_descriptor p = iter->second;
-    _edges.erase(iter);
-
-    // Invoke the eraser to remove the corresponding global property.
-    erase(p);
-}
-
-#undef BOOST_GRAPH_IS_PARAMS
-
-#if 0
-
-template <typename E>
-void
-incidence_set<E>::add(edge_descriptor e)
-{
-    this->_store.insert(e);
-}
-
-template <typename E>
-typename incidence_set<E>::iterator
-incidence_set<E>::find(edge_descriptor e)
-{
-    return this->_store.find(e);
-}
-
-template <typename E>
-typename incidence_set<E>::const_iterator
-incidence_set<E>::find(edge_descriptor e) const
-{
-    return this->_store.find(e);
-}
-
-template <typename E>
-void
-incidence_set<E>::clear()
-{
-    this->_store.clear();
-}
-
-template <typename E>
-typename incidence_set<E>::size_type
-incidence_set<E>::size() const
-{
-    return this->_store.size();
-}
-
-#endif
 
 #endif
