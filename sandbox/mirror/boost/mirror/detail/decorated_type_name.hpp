fa2884bccdc45b347ff7ec901ec9f4ac6547c494
Matus Chochlik <chochlik@gmail.com>
2008-07-05 11:11:34 +0000

    - Fixed multi-dimensional array type name building - Added build_name(...) member function to the meta_type template - Updated the registering/types.cpp example - Added BOOST_MIRROR_REG_TYPE_GS as shorthand for BOOST_MIRROR_REG_TYPE_GLOBAL_SCOPE - Added BOOST_MIRROR_REG_TYPEEF_GS and for BOOST_MIRROR_REG_TYPEDEF_GLOBAL_SCOPE for registering global scope typedefs - Added BOOST_MIRROR_REG_TYPE_EMBEDDED and for BOOST_MIRROR_REG_TYPE_EMB for registering embedded types

--- sandbox/mirror/boost/mirror/detail/decorated_type_name.hpp
+++ sandbox/mirror/boost/mirror/detail/decorated_type_name.hpp
@@ -24,28 +24,37 @@ struct decorated_type_name
 {
 private:
 	template <bool FullName>
-	inline static bstring init_name(
-		mpl::bool_<FullName> _full,
-		const bstring& left, 
-		const bstring& right
-	)
+	inline static bstring init_name(mpl::bool_<FullName> full_or_base)
 	{
-		bstring res(Decorator::prefix());
-		res.append(MetaType::get_name(_full));
-		res.append(Decorator::postfix());
-		return res;
+		bstring left;
+		bstring right;
+		bstring temp(build_name(full_or_base, left, right));
+		left.append(temp);
+		left.append(right);
+		return left;
 	}
+
+
 public:
 	template <bool FullName>
-	static const bstring& get_name(
-		mpl::bool_<FullName> _full,
-		const bstring& left = bstring(), 
-		const bstring& right = bstring()
+	inline static bstring build_name(
+		mpl::bool_<FullName> full_or_base,
+		bstring& left, 
+		bstring& right
 	)
 	{
-		static bstring s_name(
-			init_name(_full, left, right)
+		Decorator D(left, right);
+		return MetaType::build_name(
+			full_or_base,
+			left,
+			right
 		);
+	}
+
+	template <bool FullName>
+	static const bstring& get_name(mpl::bool_<FullName> full_or_base)
+	{
+		static bstring s_name(init_name(full_or_base));
 		return s_name;
 	}
 
@@ -65,20 +74,83 @@ public:
 template <typename T>
 struct type_name_decorator
 {
-	inline static bstring  prefix(void){return bstring();}
-	inline static bstring postfix(void){return bstring();}
+	inline type_name_decorator(bstring&, bstring&);
+};
+
+struct type_name_right_postfix_decorator
+{
+	inline type_name_right_postfix_decorator(bstring& _r, const bchar* _pfx)
+	: right(_r), postfix(_pfx) { }
+
+	inline ~type_name_right_postfix_decorator(void)
+	{
+		right.append(bstring(postfix));
+	}
+	bstring& right;
+	const bchar* postfix;
 };
 
 // pointer decorator
 template <typename T>
 struct type_name_decorator<T*>
+: type_name_right_postfix_decorator
 {
-	inline static bstring  prefix(void){return bstring();}
-	inline static bstring postfix(void)
+	inline type_name_decorator(bstring&, bstring& _right)
+	: type_name_right_postfix_decorator(_right, BOOST_STR_LIT(" *"))
+	{ }
+};
+
+
+// reference decorator
+template <typename T>
+struct type_name_decorator<T&>
+: type_name_right_postfix_decorator
+{
+	inline type_name_decorator(bstring&, bstring& _right)
+	: type_name_right_postfix_decorator(_right, BOOST_STR_LIT(" &"))
+	{ }
+};
+
+// const type decorator
+template <typename T>
+struct type_name_decorator<const T>
+: type_name_right_postfix_decorator
+{
+	inline type_name_decorator(bstring&, bstring& _right)
+	: type_name_right_postfix_decorator(_right, BOOST_STR_LIT(" const"))
+	{ }
+};
+
+// volatile type decorator
+template <typename T>
+struct type_name_decorator<volatile T>
+: type_name_right_postfix_decorator
+{
+	inline type_name_decorator(bstring&, bstring& _right)
+	: type_name_right_postfix_decorator(_right, BOOST_STR_LIT(" volatile"))
+	{ }
+};
+
+// const volatile type decorator
+template <typename T>
+struct type_name_decorator<const volatile T>
+: type_name_right_postfix_decorator
+{
+	inline type_name_decorator(bstring&, bstring& _r)
+	: type_name_right_postfix_decorator(_r, BOOST_STR_LIT(" const volatile"))
+	{ }
+};
+
+// array decorator
+template <typename T>
+struct type_name_decorator< T[] >
+{
+	inline type_name_decorator(bstring&, bstring& _right)
 	{
-		return bstring(BOOST_STR_LIT(" *"));
+		_right.append(BOOST_STR_LIT(" []"));
 	}
 };
+
 
 // array decorator
 template <typename T, size_t Size>
@@ -104,67 +176,10 @@ private:
 		return res;
 	}
 public:
-	inline static bstring  prefix(void){return bstring();}
-	inline static const bstring& postfix(void)
+	inline type_name_decorator(bstring&, bstring& _right)
 	{
 		static bstring s_postfix(init_postfix());
-		return s_postfix;
-	}
-};
-
-// array decorator
-template <typename T>
-struct type_name_decorator< T[] >
-{
-	inline static bstring  prefix(void){return bstring();}
-	inline static bstring postfix(void)
-	{
-		return bstring(BOOST_STR_LIT(" []"));
-	}
-};
-
-
-// reference decorator
-template <typename T>
-struct type_name_decorator<T&>
-{
-	inline static bstring  prefix(void){return bstring();}
-	inline static bstring postfix(void)
-	{
-		return bstring(BOOST_STR_LIT(" &"));
-	}
-};
-
-// const type decorator
-template <typename T>
-struct type_name_decorator<const T>
-{
-	inline static bstring  prefix(void){return bstring();}
-	inline static bstring postfix(void)
-	{
-		return bstring(BOOST_STR_LIT(" const"));
-	}
-};
-
-// volatile type decorator
-template <typename T>
-struct type_name_decorator<volatile T>
-{
-	inline static bstring  prefix(void){return bstring();}
-	inline static bstring postfix(void)
-	{
-		return bstring(BOOST_STR_LIT(" volatile"));
-	}
-};
-
-// const volatile type decorator
-template <typename T>
-struct type_name_decorator<const volatile T>
-{
-	inline static bstring  prefix(void){return bstring();}
-	inline static bstring postfix(void)
-	{
-		return bstring(BOOST_STR_LIT(" const volatile"));
+		_right.append(s_postfix);
 	}
 };
 
