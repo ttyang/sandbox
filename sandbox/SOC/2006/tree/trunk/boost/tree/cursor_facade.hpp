2c77372ab69e8a092a69402c0e408f8686e00c97
Bernhard Reiter <ockham@gmx.net>
2008-09-17 22:05:38 +0000

    Replace tabs by spaces.

--- sandbox/SOC/2006/tree/trunk/boost/tree/cursor_facade.hpp
+++ sandbox/SOC/2006/tree/trunk/boost/tree/cursor_facade.hpp
@@ -32,53 +32,53 @@ using boost::iterator_core_access;
 class cursor_core_access {
  public:
  
- 	friend class iterator_core_access;
- 	
-  	template <class Facade>
-	static bool empty_(Facade const& f)
-	{
-		return f.empty_();
-	}
-	
- 	template <class Facade>
-	static typename Facade::size_type size_(Facade const& f)
-	{
-		return f.size_();
-	}
-	
-	template <class Facade>
-	static typename Facade::size_type max_size_(Facade const& f)
-	{
-		return f.max_size_();
-	}
+     friend class iterator_core_access;
+     
+      template <class Facade>
+    static bool empty_(Facade const& f)
+    {
+        return f.empty_();
+    }
+    
+     template <class Facade>
+    static typename Facade::size_type size_(Facade const& f)
+    {
+        return f.size_();
+    }
+    
+    template <class Facade>
+    static typename Facade::size_type max_size_(Facade const& f)
+    {
+        return f.max_size_();
+    }
 
-	template <class Facade>
-	static typename Facade::size_type par(Facade const& f)
-	{
-		return f.par();
-	}
-		
-	template <class Facade>
-	static void left(Facade& f)
-	{
-		f.left();
-	}
-	
-	template <class Facade>
-	static void right(Facade& f)
-	{
-		f.right();
-	}
-	
-	//only if ascending
-	template <class Facade>
-	static void up(Facade& f)
-	{
-		f.up();
-	}
+    template <class Facade>
+    static typename Facade::size_type par(Facade const& f)
+    {
+        return f.par();
+    }
+        
+    template <class Facade>
+    static void left(Facade& f)
+    {
+        f.left();
+    }
+    
+    template <class Facade>
+    static void right(Facade& f)
+    {
+        f.right();
+    }
+    
+    //only if ascending
+    template <class Facade>
+    static void up(Facade& f)
+    {
+        f.up();
+    }
 
 private:
-	cursor_core_access();
+    cursor_core_access();
 };
 
 template <
@@ -88,109 +88,109 @@ template <
   , class VerticalCategoryOrTraversal
   , class Reference   = Value&
   , class Difference  = std::ptrdiff_t
-  , class Size		  = std::size_t
+  , class Size          = std::size_t
 >
 class cursor_facade 
  : public iterator_facade<Derived, Value, HorizontalCategoryOrTraversal, 
-						  Reference, Difference> {
+                          Reference, Difference> {
  private:
-	// Curiously Recurring Template interface.
+    // Curiously Recurring Template interface.
 
-	Derived& derived()
-	{
-	    return *static_cast<Derived*>(this);
-	}
-	
-	Derived const& derived() const
-	{
-	    return *static_cast<Derived const*>(this);
-	}
+    Derived& derived()
+    {
+        return *static_cast<Derived*>(this);
+    }
+    
+    Derived const& derived() const
+    {
+        return *static_cast<Derived const*>(this);
+    }
 
-	typedef typename cursor_facade::iterator_facade_ iterator_facade_;
+    typedef typename cursor_facade::iterator_facade_ iterator_facade_;
 
  protected:
- 	// For use by derived classes
-	typedef cursor_facade<Derived, Value, HorizontalCategoryOrTraversal,
-						  VerticalCategoryOrTraversal, Reference, Difference> 
-			cursor_facade_;
+     // For use by derived classes
+    typedef cursor_facade<Derived, Value, HorizontalCategoryOrTraversal,
+                          VerticalCategoryOrTraversal, Reference, Difference> 
+            cursor_facade_;
  public:
-	typedef typename iterator_facade_::value_type value_type;
-	typedef Reference reference;
-	typedef Difference difference_type;
-	typedef typename iterator_facade_::pointer pointer;
-	typedef typename iterator_facade_::iterator_category iterator_category;
+    typedef typename iterator_facade_::value_type value_type;
+    typedef Reference reference;
+    typedef Difference difference_type;
+    typedef typename iterator_facade_::pointer pointer;
+    typedef typename iterator_facade_::iterator_category iterator_category;
 
-	typedef Size size_type;
+    typedef Size size_type;
 
-	typedef bidirectional_traversal_tag cursor_category; //TODO
-	typedef typename 
-		iterator_category_to_traversal<iterator_category>::type
-		horizontal_traversal; 
-	typedef typename 
-		iterator_category_to_traversal<VerticalCategoryOrTraversal>::type
-		vertical_traversal; 
+    typedef bidirectional_traversal_tag cursor_category; //TODO
+    typedef typename 
+        iterator_category_to_traversal<iterator_category>::type
+        horizontal_traversal; 
+    typedef typename 
+        iterator_category_to_traversal<VerticalCategoryOrTraversal>::type
+        vertical_traversal; 
 
-	bool const empty() const
-	{
-		return cursor_core_access::empty_(this->derived());
-	}
-	
-	size_type const size() const
-	{
-		return cursor_core_access::size_(this->derived());
-	}
-	
-	size_type const max_size() const
-	{
-		return cursor_core_access::max_size_(this->derived());
-	}
+    bool const empty() const
+    {
+        return cursor_core_access::empty_(this->derived());
+    }
+    
+    size_type const size() const
+    {
+        return cursor_core_access::size_(this->derived());
+    }
+    
+    size_type const max_size() const
+    {
+        return cursor_core_access::max_size_(this->derived());
+    }
 
-	size_type const parity() const
-	{
-		return cursor_core_access::par(this->derived());
-	}
+    size_type const parity() const
+    {
+        return cursor_core_access::par(this->derived());
+    }
 
- 	Derived& to_begin()
- 	{
-		cursor_core_access::left(this->derived());
-		return this->derived();
- 	}
- 				 
- 	Derived begin()
- 	{
- 		Derived tmp(this->derived());
- 		return tmp.to_begin();
- 	}
+     Derived& to_begin()
+     {
+        cursor_core_access::left(this->derived());
+        return this->derived();
+     }
+                  
+     Derived begin()
+     {
+         Derived tmp(this->derived());
+         return tmp.to_begin();
+     }
 
- 	Derived& to_end()
- 	{
-		cursor_core_access::right(this->derived());
-		return this->derived();
- 	}
- 	
- 	Derived end()
- 	{
- 		Derived tmp(this->derived());
- 		return tmp.to_end();
-  	}
- 	
- 	Derived& to_parent()
- 	{
- 		cursor_core_access::up(this->derived());
- 		return this->derived();
- 	}
- 	
- 	Derived parent()
- 	{
- 		Derived tmp(this->derived());
- 		return tmp.to_parent();
- 	}
+     Derived& to_end()
+     {
+        cursor_core_access::right(this->derived());
+        return this->derived();
+     }
+     
+     Derived end()
+     {
+         Derived tmp(this->derived());
+         return tmp.to_end();
+      }
+     
+     Derived& to_parent()
+     {
+         cursor_core_access::up(this->derived());
+         return this->derived();
+     }
+     
+     Derived parent()
+     {
+         Derived tmp(this->derived());
+         return tmp.to_parent();
+     }
 };
 
 //TODO: Put somewhere else?
 template <class Value>
 struct metadata {
-	struct type {};
+    struct type {};
 };
 
 } // namespace tree
