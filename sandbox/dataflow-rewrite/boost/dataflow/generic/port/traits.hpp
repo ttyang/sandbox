8ea641b5e0752bce84b23a4b5ae8554411ba8def
Stjepan Rajko <stipe@asu.edu>
2008-10-15 01:36:37 +0000

    add blueprint, port_binary_operation

--- sandbox/dataflow-rewrite/boost/dataflow/generic/port/traits.hpp
+++ sandbox/dataflow-rewrite/boost/dataflow/generic/port/traits.hpp
@@ -1,7 +1,10 @@
-// Copyright 2007 Stjepan Rajko.
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+/*=================================---------------------------------------------
+    Copyright 2007,2008 Stjepan Rajko
+  
+    Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+-----------------------------------------------===============================*/
 
 #ifndef BOOST__DATAFLOW__GENERIC__PORT__TRAITS_HPP
 #define BOOST__DATAFLOW__GENERIC__PORT__TRAITS_HPP
--- sandbox/dataflow-rewrite/libs/dataflow/test/generic/test_port.cpp
+++ sandbox/dataflow-rewrite/libs/dataflow/test/generic/test_port.cpp
@@ -1,20 +1,21 @@
-// Copyright Stjepan Rajko 2007, 2008. Use, modification and
-// distribution is subject to the Boost Software License, Version
-// 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+/*=================================---------------------------------------------
+    Copyright 2007,2008 Stjepan Rajko
+  
+    Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+-----------------------------------------------===============================*/
 
 
 #include <boost/dataflow/generic/port/port.hpp>
-
 #include <boost/dataflow/generic/framework_entity.hpp>
+#include "my_framework.hpp"
 
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
 namespace df=boost::dataflow;
 
-struct my_framework : public df::framework_base
-{};
 
 struct my_port : public df::port<df::port_traits<df::ports::producer, my_framework> >
 {};
