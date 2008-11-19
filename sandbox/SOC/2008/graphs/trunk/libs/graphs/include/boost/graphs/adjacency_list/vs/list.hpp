93851e146fb279788746d8bcd86cb0fe02eb571e
Andrew Sutton <andrew.n.sutton@gmail.com>
2008-11-19 15:11:05 +0000

    Renamed a bunch of files

--- sandbox/SOC/2008/graphs/trunk/libs/graphs/include/boost/graphs/adjacency_list/vs/list.hpp
+++ sandbox/SOC/2008/graphs/trunk/libs/graphs/include/boost/graphs/adjacency_list/vs/list.hpp
@@ -1,11 +1,11 @@
 
-#ifndef BOOST_GRAPHS_ADJLIST_VERTEX_VECTOR_HPP
-#define BOOST_GRAPHS_ADJLIST_VERTEX_VECTOR_HPP
+#ifndef BOOST_GRAPHS_ADJLIST_VS_LIST_HPP
+#define BOOST_GRAPHS_ADJLIST_VS_LIST_HPP
 
-#include <vector>
-#include <algorithm>
+#include <list>
 
 #include <boost/none.hpp>
+#include <boost/counted_list.hpp>
 #include <boost/graphs/adjacency_list/vertex_store.hpp>
 
 namespace boost { namespace graphs { namespace adjacency_list {
@@ -14,25 +14,24 @@ namespace boost { namespace graphs { namespace adjacency_list {
  * @param Alloc A unary template class that will allocate stored vertices.
  */
 template <template <typename> class Alloc = std::allocator>
-struct vertex_vector
+struct vertex_list
 {
     typedef unused key_type;
 
     typedef typename descriptor_traits<
-        std::vector<int, Alloc<int>>
+        std::list<int, Alloc<int>>
     >::descriptor_type vertex_descriptor;
 
-    // The store metafunction generates the type used to store vertices in
-    // either a directed or undirected graph. This metafunction takes the
-    // fully configured vertex type as a type parameter.
     template <typename Vertex>
     struct vertex_store
     {
         typedef Alloc<Vertex> allocator;
-        typedef std::vector<Vertex, allocator> type;
+        typedef counted_list<Vertex, allocator> type;
     };
 };
+
 
 } } } /* namespace boost::graphs::adjacency_list */
 
 #endif
+
--- sandbox/SOC/2008/graphs/trunk/libs/graphs/include/boost/graphs/adjacency_list/vs/vector.hpp
+++ sandbox/SOC/2008/graphs/trunk/libs/graphs/include/boost/graphs/adjacency_list/vs/vector.hpp
@@ -1,11 +1,11 @@
 
-#ifndef BOOST_GRAPHS_ADJLIST_VERTEX_LIST_HPP
-#define BOOST_GRAPHS_ADJLIST_VERTEX_LIST_HPP
+#ifndef BOOST_GRAPHS_ADJLIST_VS_VECTOR_HPP
+#define BOOST_GRAPHS_ADJLIST_VS_VECTOR_HPP
 
-#include <list>
+#include <vector>
+#include <algorithm>
 
 #include <boost/none.hpp>
-#include <boost/counted_list.hpp>
 #include <boost/graphs/adjacency_list/vertex_store.hpp>
 
 namespace boost { namespace graphs { namespace adjacency_list {
@@ -14,24 +14,25 @@ namespace boost { namespace graphs { namespace adjacency_list {
  * @param Alloc A unary template class that will allocate stored vertices.
  */
 template <template <typename> class Alloc = std::allocator>
-struct vertex_list
+struct vertex_vector
 {
     typedef unused key_type;
 
     typedef typename descriptor_traits<
-        std::list<int, Alloc<int>>
+        std::vector<int, Alloc<int>>
     >::descriptor_type vertex_descriptor;
 
+    // The store metafunction generates the type used to store vertices in
+    // either a directed or undirected graph. This metafunction takes the
+    // fully configured vertex type as a type parameter.
     template <typename Vertex>
     struct vertex_store
     {
         typedef Alloc<Vertex> allocator;
-        typedef counted_list<Vertex, allocator> type;
+        typedef std::vector<Vertex, allocator> type;
     };
 };
-
 
 } } } /* namespace boost::graphs::adjacency_list */
 
 #endif
-
