7ad04304d7b2099c6e23fa3901718c9f9c9dc6db
Mathias Gaunard <loufoque@gmail.com>
2009-05-27 23:13:31 +0000

    renaming of pack to pipe + bounded buffer optimization using introspection

---
+++ sandbox/SOC/2009/unicode/boost/iterator/pipe_iterator.hpp
@@ -0,0 +1,278 @@
+#ifndef BOOST_PIPE_ITERATOR_HPP
+#define BOOST_PIPE_ITERATOR_HPP
+
+#include <boost/iterator/iterator_facade.hpp>
+#include <iterator>
+#include <vector>
+#include <utility>
+
+#include <boost/range.hpp>
+#include <boost/assert.hpp>
+
+#include <boost/introspection/has_member_data.hpp>
+#include <boost/utility/enable_if.hpp>
+
+namespace boost
+{
+namespace detail
+{
+    BOOST_HAS_STATIC_MEMBER_DATA(const int, max_output)
+
+    template<typename P, typename Enable = void>
+    struct pipe_output_storage;
+
+    template<typename P>
+    struct pipe_output_storage<P, typename ::boost::disable_if< has_static_member_data_max_output<P> >::type>
+    {
+private:
+        typedef std::vector<typename P::output_type> Values;
+public:
+        typedef std::back_insert_iterator<Values> output_iterator;
+        
+        const typename P::output_value& operator[](size_t i) const
+        {
+            return values[i];
+        }
+        
+        size_t last_index() const
+        {
+            return values.size() - 1;
+        }
+        
+        output_iterator out()
+        {
+            values.clear();
+            return std::back_inserter(values);
+        }
+        
+        void update(output_iterator)
+        {
+        }
+        
+    private:
+        Values values;
+    };
+    
+    template<typename P>
+    struct pipe_output_storage<P, typename boost::enable_if< has_static_member_data_max_output<P> >::type>
+    {
+private:
+        typedef typename P::output_type Value;
+public:
+        typedef Value* output_iterator;
+        
+        const Value& operator[](size_t i) const
+        {
+            return values[i];
+        }
+        
+        size_t last_index() const
+        {
+            return last;
+        }
+        
+        output_iterator out()
+        {
+            return values;
+        }
+        
+        void update(output_iterator u)
+        {
+            last = u - values - 1;
+        }
+        
+    private:
+        Value values[P::max_output];
+        size_t last;
+    };
+}
+
+template<typename P>
+struct one_many_pipe : P
+{
+	one_many_pipe(P p_) : P(p_)
+	{
+	}
+	
+	template<typename In, typename Out>
+	std::pair<In, Out>
+	ltr(In begin, In end, Out out)
+	{
+		BOOST_ASSERT(begin != end);
+		
+		out = (*this)(*begin, out);
+		return std::make_pair(++begin, out);
+	}
+	
+	template<typename In, typename Out>
+	std::pair<In, Out>
+	rtl(In begin, In end, Out out)
+	{
+		BOOST_ASSERT(begin != end);
+		
+		out = (*this)(*--end, out);
+		return std::make_pair(end, out);
+	}
+};
+
+template<typename P>
+one_many_pipe<P> make_one_many_pipe(P p)
+{
+	return one_many_pipe<P>(p);
+}
+
+template<typename It, typename Pipe>
+struct pipe_iterator
+	: boost::iterator_facade<
+		pipe_iterator<It, Pipe>,
+		typename Pipe::output_type,
+		std::bidirectional_iterator_tag,
+		const typename Pipe::output_type
+	>
+{
+	pipe_iterator(It begin_, It pos_, It end_, Pipe p_) : pos(pos_), begin(begin_), end(end_), index(0), p(p_)
+	{
+		if(pos != end)
+        {
+            std::pair<It, typename detail::pipe_output_storage<Pipe>::output_iterator> pair =
+                p.ltr(pos, end, values.out());
+            next_pos = pair.first;
+            values.update(pair.second);
+        }
+	}
+	
+	It base() const
+	{
+		return pos;
+	}
+	
+private:
+	typedef typename Pipe::output_type T;
+
+	friend class boost::iterator_core_access;
+
+	T dereference() const
+	{
+		return values[index];
+	}
+	
+	void increment()
+	{
+		if(index != values.last_index())
+		{
+			index++;
+		}
+		else
+		{			
+			pos = next_pos;	
+			if(pos != end)
+            {
+                std::pair<It, typename detail::pipe_output_storage<Pipe>::output_iterator> pair =
+				    p.ltr(pos, end, values.out());
+                next_pos = pair.first;
+                values.update(pair.second);
+            }
+			index = 0;
+		}
+	}
+	
+	void decrement()
+	{
+		if(index != 0)
+		{
+			index--;
+		}
+		else
+		{
+			next_pos = pos;	
+            
+            std::pair<It, typename detail::pipe_output_storage<Pipe>::output_iterator> pair =
+                p.rtl(begin, pos, values.out());
+            pos = pair.first;
+            values.update(pair.second);
+            
+			index = values.last_index();
+		}
+	}
+	
+	bool equal(const pipe_iterator& other) const
+	{
+		return pos == other.pos && index == other.index;
+	}
+	
+	It pos;
+	It next_pos;
+	
+	It begin;
+	It end;
+	size_t index;
+	
+	Pipe p;
+	
+	detail::pipe_output_storage<Pipe> values;
+};
+
+template<typename It, typename P>
+pipe_iterator<It, P> make_pipe_iterator(It begin, It pos, It end, P p)
+{
+	return pipe_iterator<It, P>(begin, pos, end, p);
+}
+
+template<typename Range, typename P>
+std::pair<
+	pipe_iterator<typename boost::range_iterator<const Range>::type, P>,
+	pipe_iterator<typename boost::range_iterator<const Range>::type, P>
+> make_pipe_range(const Range& range, P p)
+{
+	return std::make_pair(
+		make_pipe_iterator(boost::begin(range), boost::begin(range), boost::end(range), p),
+		make_pipe_iterator(boost::begin(range), boost::end(range), boost::end(range), p)
+	);
+}
+
+template<typename It, typename Pipe>
+struct pipe_output_iterator
+{
+	pipe_output_iterator(It pos_, Pipe p_) : pos(pos_), p(p_)
+	{
+	}
+	
+	It base() const
+	{
+		return pos;
+	}
+	
+	pipe_output_iterator& operator*() const
+	{
+		return const_cast<pipe_output_iterator&>(*this);
+	}
+	
+	pipe_output_iterator& operator++()
+	{
+		return *this;
+	}
+	
+	pipe_output_iterator& operator++(int)
+	{
+		return *this;
+	}
+	
+	void operator=(typename Pipe::output_type val) const
+	{
+		pos = p.ltr(&val, &val + 1, pos).second;
+	}
+	
+private:	
+	It pos;
+	Pipe p;
+};
+
+template<typename OutputIterator, typename P>
+pipe_output_iterator<OutputIterator, P> make_pipe_output_iterator(OutputIterator out, P p)
+{
+	return pipe_output_iterator<OutputIterator, P>(out, p);
+}
+
+} // namespace boost
+
+#endif
---
+++ sandbox/SOC/2009/unicode/boost/unicode/utf_conversion.hpp
@@ -0,0 +1,354 @@
+#ifndef BOOST_UTF_CONVERSION_HPP
+#define BOOST_UTF_CONVERSION_HPP
+
+#include <boost/assert.hpp>
+#include <boost/throw_exception.hpp>
+#include <stdexcept>
+#ifndef BOOST_NO_STD_LOCALE
+#include <sstream>
+#include <ios>
+#endif
+
+#include <boost/iterator/pipe_iterator.hpp>
+#include <boost/unicode/unicode_proprieties.hpp>
+
+namespace boost
+{
+
+namespace detail
+{
+    
+static const char16 high_surrogate_base = 0xD7C0u;
+static const char16 low_surrogate_base = 0xDC00u;
+static const char32 ten_bit_mask = 0x3FFu;
+
+inline unsigned utf8_byte_count(uint8_t c)
+{
+   // if the most significant bit with a zero in it is in position
+   // 8-N then there are N bytes in this UTF-8 sequence:
+   uint8_t mask = 0x80u;
+   unsigned result = 0;
+   while(c & mask)
+   {
+      ++result;
+      mask >>= 1;
+   }
+   return (result == 0) ? 1 : ((result > 4) ? 4 : result);
+}
+
+inline unsigned utf8_trailing_byte_count(uint8_t c)
+{
+   return utf8_byte_count(c) - 1;
+}
+
+inline void invalid_code_point(char32 val)
+{
+#ifndef BOOST_NO_STD_LOCALE
+	std::stringstream ss;
+	ss << "Invalid UTF-32 code point U+" << std::showbase << std::hex << (uint_least32_t)val << " encountered while trying to encode UTF-16 sequence";
+	std::out_of_range e(ss.str());
+#else
+	std::out_of_range e("Invalid UTF-32 code point encountered while trying to encode UTF-16 sequence");
+#endif
+	throw_exception(e);
+}
+
+template<typename Iterator>
+inline void invalid_utf_sequence(Iterator begin, Iterator end)
+{
+#ifndef BOOST_NO_STD_LOCALE
+	std::stringstream ss;
+	ss << "Invalid UTF sequence " << std::showbase << std::hex;
+	for(Iterator it = begin; it != end; ++it)
+		ss << *it << " ";
+	ss << "encountered while trying to decode UTF-32 sequence";
+	std::out_of_range e(ss.str());
+#else
+	std::out_of_range e("Invalid UTF sequence encountered while trying to decode UTF-32 sequence");
+#endif
+	throw_exception(e);
+}
+
+struct u16_packer
+{
+	typedef char16 output_type;
+    static const int max_output = 2;
+	
+	template<typename OutputIterator>
+	OutputIterator operator()(char32 v, OutputIterator out)
+	{
+		if(v >= 0x10000u)
+		{
+			if(v > 0x10FFFFu)
+				detail::invalid_code_point(v);
+				
+			// split into two surrogates:
+			output_type hi = static_cast<output_type>(v >> 10) + detail::high_surrogate_base;
+			output_type lo = static_cast<output_type>(v & detail::ten_bit_mask) + detail::low_surrogate_base;
+			
+			BOOST_ASSERT(unicode::is_high_surrogate(hi));
+			BOOST_ASSERT(unicode::is_low_surrogate(lo));
+			
+			*out++ = hi;
+			*out++ = lo;
+		}
+		else
+		{
+			// 16-bit code point:
+			output_type cp = static_cast<output_type>(v);
+
+			// value must not be a surrogate:
+			if(unicode::is_surrogate(cp))
+				detail::invalid_code_point(v);
+				
+			*out++ = cp;
+		}
+		
+		return out;
+	}
+};
+
+struct u16_unpacker
+{
+	typedef char32 output_type;
+    static const int max_output = 1;
+	
+	template<typename In, typename Out>
+	std::pair<In, Out>
+	ltr(In begin, In end, Out out)
+	{
+		BOOST_ASSERT(begin != end);
+		
+		In it = begin;
+		char32 value = *it;
+		
+		if(unicode::is_high_surrogate(value))
+		{
+			if(++it == end)
+				detail::invalid_utf_sequence(begin, end);
+			
+			// precondition; next value must have be a low-surrogate:
+         	char16 lo = *it;
+         	if(!unicode::is_low_surrogate(lo))
+            	detail::invalid_code_point(lo);
+				
+         	value = code_point(value, lo);
+      	}
+      	// postcondition; result must not be a surrogate:
+      	if(unicode::is_surrogate(value))
+			detail::invalid_code_point(static_cast<char16>(value));
+		
+		*out++ = value;
+				
+		return std::make_pair(++it, out);
+	}
+	
+	template<typename In, typename Out>
+	std::pair<In, Out>
+	rtl(In begin, In end, Out out)
+	{
+		BOOST_ASSERT(begin != end);
+		
+		In it = --end;
+		char32 value = *it;
+		
+		if(unicode::is_low_surrogate(value))
+		{
+			if(it == begin)
+				invalid_utf_sequence(begin, end);
+			--it;
+			
+			char16 hi = *it;
+         	if(!unicode::is_high_surrogate(hi))
+            	invalid_code_point(hi);
+			
+			value = code_point(hi, value);
+      	}
+      	// postcondition; result must not be a surrogate:
+      	if(unicode::is_surrogate(value))
+			invalid_code_point(static_cast<char16>(value));
+			
+		*out++ = value;
+		
+		return std::make_pair(it, out);
+	}
+	
+private:
+	char32 code_point(char16 hi, char16 lo)
+	{
+		return   ((hi - detail::high_surrogate_base) << 10)
+		       | (static_cast<char32>(static_cast<char16>(lo)) & detail::ten_bit_mask);
+	}
+};
+
+struct u8_packer
+{
+	typedef char output_type;
+    static const int max_output = 4;
+	
+	template<typename OutputIterator>
+	OutputIterator operator()(char32 c, OutputIterator out)
+	{
+		if(c > 0x10FFFFu)
+			detail::invalid_code_point(c);
+
+		if(c < 0x80u)
+		{
+			*out++ = static_cast<unsigned char>(c);
+		}
+		else if(c < 0x800u)
+		{
+			*out++ = static_cast<unsigned char>(0xC0u + (c >> 6));
+			*out++ = static_cast<unsigned char>(0x80u + (c & 0x3Fu));
+		}
+		else if(c < 0x10000u)
+		{
+			*out++ = static_cast<unsigned char>(0xE0u + (c >> 12));
+			*out++ = static_cast<unsigned char>(0x80u + ((c >> 6) & 0x3Fu));
+			*out++ = static_cast<unsigned char>(0x80u + (c & 0x3Fu));
+		}
+		else
+		{
+			*out++ = static_cast<unsigned char>(0xF0u + (c >> 18));
+			*out++ = static_cast<unsigned char>(0x80u + ((c >> 12) & 0x3Fu));
+			*out++ = static_cast<unsigned char>(0x80u + ((c >> 6) & 0x3Fu));
+			*out++ = static_cast<unsigned char>(0x80u + (c & 0x3Fu));
+		}
+		
+		return out;
+	}
+};
+
+struct u8_unpacker
+{
+	typedef char32 output_type;
+    static const int max_output = 1;
+	
+	template<typename In, typename Out>
+	std::pair<In, Out>
+	ltr(In begin, In end, Out out)
+	{
+		BOOST_ASSERT(begin != end);
+		
+		In it = begin;
+		char32 value = *it;
+		
+		if((value & 0xC0u) == 0x80u)
+			detail::invalid_utf_sequence(begin, end);
+			
+		// see how many extra byts we have:
+		unsigned extra = detail::utf8_trailing_byte_count(value);
+		// extract the extra bits, 6 from each extra byte:
+		for(unsigned c = 0; c < extra; ++c)
+		{
+			if(++it == end)
+				detail::invalid_utf_sequence(begin, end);
+			
+			value <<= 6;
+			value += static_cast<unsigned char>(*it) & 0x3Fu;
+		}
+		
+		// we now need to remove a few of the leftmost bits, but how many depends
+		// upon how many extra bytes we've extracted:
+		static const char32 masks[4] = 
+		{
+			0x7Fu,
+			0x7FFu,
+			0xFFFFu,
+			0x1FFFFFu,
+		};
+		value &= masks[extra];
+		
+		// check the result:
+		if(value > static_cast<char32>(0x10FFFFu))
+			invalid_utf_sequence(begin, end);
+		
+		*out++ = value;
+				
+		return std::make_pair(++it, out);
+	}
+	
+	template<typename In, typename Out>
+	std::pair<In, Out>
+	rtl(In begin, In end, Out out)
+	{
+		BOOST_ASSERT(begin != end);
+		
+		In it = --end;
+		char32 value = *it;
+		
+		// Keep backtracking until we don't have a trailing character:
+		unsigned count = 0;
+		while((*it & 0xC0u) == 0x80u)
+		{					
+			if(count >= 4 || it == begin)
+				invalid_utf_sequence(begin, end);
+				
+			--it;
+			++count;
+		}
+
+		// now check that the sequence was valid:
+		if(count != detail::utf8_trailing_byte_count(value))
+			invalid_utf_sequence(begin, end);
+		
+		out = ltr(it, end, out).second;
+		return std::make_pair(it, out);
+	}
+};
+
+} // namespace detail
+
+template<typename Range>
+std::pair<
+	pipe_iterator<typename range_iterator<const Range>::type, one_many_pipe<detail::u16_packer> >,
+	pipe_iterator<typename range_iterator<const Range>::type, one_many_pipe<detail::u16_packer> >
+> make_u32_to_u16_range(const Range& range)
+{
+	return make_pipe_range(range, make_one_many_pipe(detail::u16_packer()));
+}
+
+template<typename Range>
+std::pair<
+	pipe_iterator<typename range_iterator<const Range>::type, detail::u16_unpacker>,
+	pipe_iterator<typename range_iterator<const Range>::type, detail::u16_unpacker>
+> make_u16_to_u32_range(const Range& range)
+{
+	return make_pipe_range(range, detail::u16_unpacker());
+}
+
+template<typename Range>
+std::pair<
+	pipe_iterator<typename range_iterator<const Range>::type, one_many_pipe<detail::u8_packer> >,
+	pipe_iterator<typename range_iterator<const Range>::type, one_many_pipe<detail::u8_packer> >
+> make_u32_to_u8_range(const Range& range)
+{
+	return make_pipe_range(range, make_one_many_pipe(detail::u8_packer()));
+}
+
+template<typename Range>
+std::pair<
+	pipe_iterator<typename range_iterator<const Range>::type, detail::u8_unpacker>,
+	pipe_iterator<typename range_iterator<const Range>::type, detail::u8_unpacker>
+> make_u8_to_u32_range(const Range& range)
+{
+	return make_pipe_range(range, detail::u8_unpacker());
+}
+
+template<typename OutputIterator>
+pipe_output_iterator<OutputIterator, one_many_pipe<detail::u8_packer> >
+make_u8_output_iterator(OutputIterator out)
+{
+	return make_pipe_output_iterator(out, make_one_many_pipe(detail::u8_packer()));
+}
+
+template<typename OutputIterator>
+pipe_output_iterator<OutputIterator, one_many_pipe<detail::u16_packer> >
+make_u16_output_iterator(OutputIterator out)
+{
+	return make_pipe_output_iterator(out, make_one_many_pipe(detail::u16_packer()));
+}
+
+} // namespace boost
+
+#endif
