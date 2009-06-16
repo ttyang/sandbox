f5d63598d342e71a6ac287df9dbb84b3e5e03ab0
Christian Schladetsch <christian.schladetsch@gmail.com>
2009-06-16 01:49:37 +0000

    changed test_map_list to use chained_storage

--- sandbox/monotonic/libs/monotonic/test/test_chained_storage.cpp
+++ sandbox/monotonic/libs/monotonic/test/test_chained_storage.cpp
@@ -5,9 +5,10 @@ void test_chained_storage()
 {
 	monotonic::chained_storage<10> store;
 	{
-		std::vector<char, monotonic::allocator<char> > vec(store);
-		vec.resize(5);
-		vec.resize(500);
+		typedef std::vector<char, monotonic::allocator<char> > Vector;
+		Vector vec(store);
+		vec.resize(5);		// still on the stack
+		vec.resize(500);	// now on the heap
 	}
 }
 
