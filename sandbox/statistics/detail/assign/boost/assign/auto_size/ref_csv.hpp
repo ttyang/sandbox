4f9ee6c858856e0220b9a6410132a8b0f4e391f0
Erwann Rogard <erwann.rogard@gmail.com>
2010-03-07 17:45:50 +0000

    m

--- sandbox/statistics/detail/assign/boost/assign/auto_size/ref_csv.hpp
+++ sandbox/statistics/detail/assign/boost/assign/auto_size/ref_csv.hpp
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////////////
-// assign::ref_csv.hpp                                              //
+// assign::ref_csv.hpp                                                      //
 //                                                                          //
 //  (C) Copyright 2010 Erwann Rogard                                        //
 //  Use, modification and distribution are subject to the                   //
