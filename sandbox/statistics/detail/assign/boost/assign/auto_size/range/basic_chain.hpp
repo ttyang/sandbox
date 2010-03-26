4989e7b81a84a4db99b7b8b2e3ae4c9fc968a2b3
Erwann Rogard <erwann.rogard@gmail.com>
2010-03-26 02:09:21 +0000

    m

--- sandbox/statistics/detail/assign/boost/assign/auto_size/range/basic_chain.hpp
+++ sandbox/statistics/detail/assign/boost/assign/auto_size/range/basic_chain.hpp
@@ -1,13 +1,13 @@
 //////////////////////////////////////////////////////////////////////////////
-// assign::detail::basic_chain_impl.hpp                                           //
+// assign::detail::basic_chain.hpp                                          //
 //                                                                          //
 //  (C) Copyright 2010 Erwann Rogard                                        //
 //  Use, modification and distribution are subject to the                   //
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_DETAIL_RANGE_CHAIN_L_ER_2010_HPP
-#define BOOST_ASSIGN_DETAIL_RANGE_CHAIN_L_ER_2010_HPP
+#ifndef BOOST_ASSIGN_DETAIL_RANGE_BASIC_CHAIN_ER_2010_HPP
+#define BOOST_ASSIGN_DETAIL_RANGE_BASIC_CHAIN_ER_2010_HPP
 #include <boost/mpl/if.hpp>
 #include <boost/mpl/void.hpp>
 #include <boost/mpl/bool.hpp>
