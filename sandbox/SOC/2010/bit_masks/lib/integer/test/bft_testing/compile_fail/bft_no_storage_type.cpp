d5a6803751993dd1c1ac180bee7aec6c7b8fb0bd
Brian Bartman <bbartmanboost@gmail.com>
2010-06-10 13:20:15 +0000

    fixed a bug in my test which was causing a compile-fail test to compile and because of that fail

--- sandbox/SOC/2010/bit_masks/lib/integer/test/bft_testing/compile_fail/bft_no_storage_type.cpp
+++ sandbox/SOC/2010/bit_masks/lib/integer/test/bft_testing/compile_fail/bft_no_storage_type.cpp
@@ -4,14 +4,16 @@
 //  http://www.boost.org/LICENSE_1_0.txt)
 
 
-#include <boost/integer/details/member.hpp>
+#include <boost/integer/bitfield_tuple.hpp>
 
 using namespace boost;
-
+struct red;
 // Fails because no storage is specified.
-
 typedef bitfield_tuple< member<int,red,3> > bft;
 
+
 int main() {
+    bft();  // don't for get in order make sure that the template works you 
+            // either need to construct it or ask for a type in it!
     return 0;
 }
