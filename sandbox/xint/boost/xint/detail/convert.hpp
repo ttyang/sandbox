4a37bbc365d6817de246c73b4736ae0b0d7dd75d
Chad Nelson <chad.thecomfychair@gmail.com>
2010-06-12 15:54:07 +0000

    Fifth Boost release. All types are consolidated into a single integer_t template type. Added Boost.Parameter options to the integer_t type.

---
+++ sandbox/xint/boost/xint/detail/convert.hpp
@@ -0,0 +1,254 @@
+
+/*
+    The Extended Integer (XInt) Library
+    A fast, portable C++ library for multi-precision integer math
+    Copyright 2010 by Chad Nelson
+
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+
+    See http://www.boost.org/libs/xint for library home page.
+*/
+
+/*! \file
+    \brief Contains the conversion functions.
+
+    This file will be included by the library itself when needed.
+*/
+
+#ifndef BOOST_INCLUDED_XINT_CONVERT_HPP
+#define BOOST_INCLUDED_XINT_CONVERT_HPP
+
+//! @cond detail
+namespace boost {
+namespace xint {
+namespace detail {
+
+inline char nToChar(int n, bool upperCase) {
+    if (n < 10) return char(n + '0');
+    else if (upperCase) return char((n - 10) + 'A');
+    else return char((n - 10) + 'a');
+}
+
+BOOST_XINT_RAWINT_TPL
+std::string to_string(const BOOST_XINT_RAWINT n, size_t base = 10, bool
+    uppercase = false)
+{
+    if (base < 2 || base > 36) throw exceptions::invalid_base();
+    if (n.is_zero()) return "0";
+
+    std::ostringstream out;
+    if (base == 2 || base == 4 || base == 16) {
+        // Fast no-division version, useful for debugging division and for cases
+        // where maximum speed is necessary.
+        const digit_t *firstDigit = n.digits(), *lastDigit = firstDigit +
+            n.length - 1;
+
+        if (n.negative) out << '-';
+
+        int bits_per_base_b_digit = 0;
+        while (base > 1) { base = base >> 1; ++bits_per_base_b_digit; }
+        const digit_t mask = (doubledigit_t(1) << bits_per_base_b_digit) - 1;
+
+        // Skip any leading zeros
+        const digit_t *d = lastDigit;
+        int digitShift = (bits_per_digit - bits_per_base_b_digit);
+        while (digitShift >= 0 && ((*d >> digitShift) & mask) == 0)
+            digitShift -= bits_per_base_b_digit;
+
+        do {
+            while (digitShift >= 0) {
+                out << nToChar((*d >> digitShift) & mask, uppercase);
+                digitShift -= bits_per_base_b_digit;
+            }
+
+            digitShift=(bits_per_digit - bits_per_base_b_digit);
+        } while (d-- != firstDigit);
+
+        return out.str();
+    } else {
+        // ATTN: for when there's nothing more pressing to do
+        // This could be made a lot more efficient for very large numbers, by
+        // dividing n by base raised to whatever number of digits will fit into
+        // a doubledigit_t, then just doing single-digit divides for that many
+        // digits before going back to n for another chunk. That would be
+        // premature optimization though, so I'm just making this note instead.
+        // If someone can show me a use-case where more efficiency is needed in
+        // this function, I'll add it later.
+
+        BOOST_XINT_RAWINT shift;
+        shift.set_unsigned(base);
+        typename BOOST_XINT_RAWINT::divide_t a(divide(n.abs(), shift));
+        do {
+            out << nToChar(a.remainder[0], uppercase);
+            a = divide(a.quotient, shift);
+        } while (!a.quotient.is_zero());
+        if (!a.remainder.is_zero()) out << nToChar(a.remainder[0], uppercase);
+
+        if (n.negative) out << '-';
+
+        std::string rval = out.str();
+        std::reverse(rval.begin(), rval.end());
+        return rval;
+    }
+}
+
+BOOST_XINT_RAWINT_TPL
+void BOOST_XINT_RAWINT::from_string(const char *str, char **endptr, size_t base)
+{
+    bool negate = false;
+    const char *c = str;
+    while (isspace(*c)) ++c;
+
+    if (*c == '+') ++c;
+    else if (*c == '-') { negate = true; ++c; }
+
+    if (base == autobase) {
+        if (*c == '0') {
+            ++c;
+            if (*c == 'x' || *c == 'X') {
+                ++c;
+                base = 16;
+            } else base = 8;
+        } else base = 10;
+    }
+
+    if (base < 2 || base > 36) throw exceptions::invalid_base();
+    if (*c == 0) throw exceptions::invalid_digit("No valid digits");
+
+    std::string tstr;
+    if (negate) tstr.push_back('-');
+    if (base <= 10) {
+        const char p = char('0' + base);
+        while (*c >= '0' && *c < p)
+            tstr.push_back(*c++);
+    } else {
+        const char lower = char('a' + base - 10), upper = char('A' + base - 10);
+        while ((*c >= '0' && *c <= '9')
+            || (*c >= 'a' && *c < lower)
+            || (*c >= 'A' && *c < upper))
+                tstr.push_back(*c++);
+    }
+    *endptr = const_cast<char*>(c);
+
+    from_string(tstr, base);
+}
+
+BOOST_XINT_RAWINT_TPL
+void BOOST_XINT_RAWINT::from_string(const char *str, size_t base) {
+    bool negate = false;
+    const char *c = str;
+    if (*c == '+') ++c;
+    else if (*c == '-') { negate = true; ++c; }
+
+    if (base == autobase) {
+        if (*c == '0') {
+            ++c;
+            if (*c == 'x' || *c == 'X') {
+                ++c;
+                base = 16;
+            } else base = 8;
+        } else base = 10;
+    }
+
+    if (base < 2 || base > 36) throw exceptions::invalid_base();
+    if (*c == 0) throw exceptions::invalid_digit("No valid digits");
+
+    // ATTN: for when there's nothing more pressing to do
+    // This function could use the same efficiency improvements that to_string
+    // uses, but there's even less need for them here. Show me a use-case where
+    // they're needed and I'll add them; until then, this will suffice.
+
+    BOOST_XINT_RAWINT shift, digit;
+    shift.set_unsigned(base);
+
+    set(0);
+    while (*c) {
+        if (*c >= '0' && *c <= '9') digit.set(*c - '0');
+        else if (*c >= 'A' && *c <= 'Z') digit.set(*c - 'A' + 10);
+        else if (*c >= 'a' && *c <= 'z') digit.set(*c - 'a' + 10);
+        else
+            throw exceptions::invalid_digit("encountered non-alphanumeric "
+                "character in string");
+
+        if (digit >= shift) throw exceptions::invalid_digit("encountered digit "
+            "greater than base allows");
+
+        *this *= shift;
+        *this += digit;
+        ++c;
+    }
+
+    negative = negate;
+    trim();
+}
+
+BOOST_XINT_RAWINT_TPL
+void BOOST_XINT_RAWINT::from_string(const std::string& str, size_t base) {
+    from_string(str.c_str(), base);
+}
+
+BOOST_XINT_RAWINT_TPL
+xint::binary_t to_binary(const BOOST_XINT_RAWINT n, size_t bits = 0) {
+    if (bits > size_t(std::numeric_limits<unsigned char>::digits)) throw
+        exceptions::invalid_argument("can't fit that many bits into an "
+        "unsigned character on this system");
+    if (bits == 0) bits = std::numeric_limits<unsigned char>::digits;
+
+    bitqueue_t bitqueue;
+    const digit_t *s = n.digits(), *se = s + n.length;
+    while (s != se) bitqueue.push(*s++, bits_per_digit);
+
+    xint::binary_t target;
+    while (!bitqueue.empty()) target.push_back(static_cast<unsigned char>
+      (bitqueue.pop(bits)));
+    while (!target.empty() && target.back()==0) target.pop_back();
+    return target;
+}
+
+BOOST_XINT_RAWINT_TPL
+void BOOST_XINT_RAWINT::from_binary(xint::binary_t b, size_t bits) {
+    if (bits > size_t(std::numeric_limits<unsigned char>::digits)) throw
+        exceptions::invalid_argument("can't fit that many bits into an "
+        "unsigned character on this system");
+    if (bits == 0) bits = std::numeric_limits<unsigned char>::digits;
+
+    bitqueue_t bitqueue;
+    for (xint::binary_t::const_iterator s = b.begin(), se = b.end(); s != se;
+        ++s) bitqueue.push(*s, bits);
+
+    digit_t *d = digits(bits_to_digits(b.size() * bits), realloc::ignore), *t =
+        d, *te = t + max_length();
+    while (t != te && !bitqueue.empty())
+      *t++ = static_cast<digit_t>(bitqueue.pop(bits_per_digit));
+    length = (t - d);
+    trim();
+}
+
+template <typename T, bitsize_t Bits, bool Secure, class Alloc>
+T to(const BOOST_XINT_RAWINT n, typename boost::enable_if<boost::is_integral<T>
+    >::type* = 0)
+{
+    using std::numeric_limits;
+
+    static const BOOST_XINT_RAWINT tmin((numeric_limits<T>::min)());
+    static const BOOST_XINT_RAWINT tmax((numeric_limits<T>::max)());
+    if (n < tmin || n > tmax) throw exceptions::too_big("value out of range "
+		"for requested conversion");
+
+    T rval = 0, shift = T(digit_overflowbit);
+    for (size_t x = 0; x < n.length; ++x) {
+        if (sizeof(T) > sizeof(digit_t)) rval *= shift;
+        rval += static_cast<T>(n[n.length - x - 1]);
+    }
+    if (n.negative) rval *= -1;
+    return rval;
+}
+
+} // namespace detail
+} // namespace xint
+} // namespace boost
+//! @endcond detail
+
+#endif // BOOST_INCLUDED_XINT_CONVERT_HPP
