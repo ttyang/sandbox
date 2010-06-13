fe479d9d6b0639bbdd90d180aeea0587e22ebcfb
Erwann Rogard <erwann.rogard@gmail.com>
2010-06-13 14:41:18 +0000

    m

---
+++ sandbox/statistics/support/libs/assign/example/range.h
@@ -0,0 +1,11 @@
+//////////////////////////////////////////////////////////////////////////////
+// :range.h                                                                 //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef LIBS_ASSIGN_EXAMPLE_RANGE_ER_2010_H
+#define LIBS_ASSIGN_EXAMPLE_RANGE_ER_2010_H
+#include <ostream>
+
+void example_range(std::ostream&);
+
+#endif
