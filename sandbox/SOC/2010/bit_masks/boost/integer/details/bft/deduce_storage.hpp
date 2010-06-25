b70864c67d54897fcd039f8648d5d842891c5806
Brian Bartman <bbartmanboost@gmail.com>
2010-06-25 18:04:58 +0000

    renaming adding license and include guards

---
+++ sandbox/SOC/2010/bit_masks/boost/integer/details/bft/deduce_storage.hpp
@@ -0,0 +1,20 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_BITFIELD_TUPLE_DEDUCED_STORAGE_HPP
+#define BOOST_BITFIELD_TUPLE_DEDUCED_STORAGE_HPP
+
+#include <boost/cstdint.hpp>
+
+namespace boost { namespace details {
+
+/** Meta functions used for deducing the type of storage that should be used if
+ *  the user doesn't supply a storage type to a bitfield_tuple.
+ */
+
+
+}} // end boost::details
+
+#endif
