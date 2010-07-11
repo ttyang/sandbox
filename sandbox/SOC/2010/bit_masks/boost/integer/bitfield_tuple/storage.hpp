7df9f290b04ef00aa8a69de5d0a3825f2d9df5b1
Brian Bartman <bbartmanboost@gmail.com>
2010-07-11 12:07:29 +0000

    re organizing my code so that header fiels are were they should be

---
+++ sandbox/SOC/2010/bit_masks/boost/integer/bitfield_tuple/storage.hpp
@@ -0,0 +1,21 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef BOOST_BIT_MASKS_TAGGED_HPP
+#define BOOST_BIT_MASKS_TAGGED_HPP
+
+namespace boost {
+namespace bitfields {
+
+/** Storage type used for moving the type to be used for interenal storage 
+ *  around within the template parameters of a bitfield_tuple.
+ */
+template < typename StorageType>
+struct storage;
+
+}} // end boost::bitfields
+
+#endif
