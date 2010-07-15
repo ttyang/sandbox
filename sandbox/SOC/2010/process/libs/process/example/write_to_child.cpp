9b49d8b9af835319694a71e978b3070b7f3ae72d
Felipe de Oliveira Tanus <fotanus@gmail.com>
2010-07-15 02:07:59 +0000

    WIN32 update.
    Restarted to track on matrix what is working and what is not.
    
    *Pipes fixed
    *Wait fixed
    *Examples update

---
+++ sandbox/SOC/2010/process/libs/process/example/write_to_child.cpp
@@ -0,0 +1,27 @@
+//
+// Boost.Process
+// ~~~~~~~~~~~~~
+//
+// Copyright (c) 2006, 2007 Julio M. Merino Vidal
+// Copyright (c) 2008, 2009 Boris Schaeling
+// Copyright (c) 2010 Felipe Tanus, Boris Schaeling
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/all/process.hpp> 
+#include <string> 
+#include <iostream> 
+
+using namespace boost::process; 
+
+int main() 
+{ 
+    std::string exe = find_executable_in_path("ftp"); 
+    context ctx; 
+    ctx.stdin_behavior = behavior::pipe::def(behavior::pipe::stream_type::input_stream); 
+    child c = create_child(exe, ctx); 
+    postream &os = c.get_stdin(); 
+    os << "quit" << std::endl; 
+} 
