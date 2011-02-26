e7aaebd07ab526a50d677c693e9f37d9271cc2b9
Erwann Rogard <erwann.rogard@gmail.com>
2011-02-26 19:06:57 +0000

    assign_v2: adding a generic csv interface and deleting that specific to particular components. ref framework not affected (yet)

---
+++ sandbox/assign_v2/boost/assign/v2/detail/config/enable_cpp0x.hpp
@@ -0,0 +1,40 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_ENABLE_CPP0X
+#include <boost/preprocessor/logical/or.hpp>
+#include <boost/config.hpp>
+#if defined(BOOST_NO_RVALUE_REFERENCES) || defined(BOOST_NO_VARIADIC_TEMPLATES) || defined(BOOST_NO_0X_HDR_TUPLE)
+#define BOOST_ASSIGN_V2_ENABLE_CPP0X 0
+#else
+#define BOOST_ASSIGN_V2_ENABLE_CPP0X 1
+#endif
+#endif
+
+
+/*
+ -- Previously,
+//#if __GNUC__ && __GXX_EXPERIMENTAL_CXX0X__
+//#define BOOST_ASSIGN_V2_ENABLE_CPP0X 1
+//#else
+//#define BOOST_ASSIGN_V2_ENABLE_CPP0X 0
+//#endif
+
+ -- Alternatively,
+
+//#include <boost/preprocessor/logical/bool.hpp>
+//#define BOOST_ASSIGN_V2_ENABLE_CPP0X BOOST_PP_BOOL(__cplusplus > 199711L)
+
+(but GCC non-conformant.)
+
+ -- Sources:
+ http:gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html#Common-Predefined-Macros
+ http:www2.research.att.com/~bs/C++0xFAQ.html
+ http:msdn.microsoft.com/en-us/library/dd465215.aspx
+*/
