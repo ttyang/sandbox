bc2e74c2581f4a6f63c3ec782b5d7ea8ac0d4fa0
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-01 00:27:00 +0000

    upd assign_v2

--- sandbox/assign_v2/boost/assign/v2/put/pipe.hpp
+++ sandbox/assign_v2/boost/assign/v2/put/pipe.hpp
@@ -7,12 +7,11 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_V2_REF_WRAPPER_ER_2010_HPP
-#define BOOST_ASSIGN_V2_REF_WRAPPER_ER_2010_HPP
+#ifndef BOOST_ASSIGN_V2_PUT_PIPE_ER_2010_HPP
+#define BOOST_ASSIGN_V2_PUT_PIPE_ER_2010_HPP
 
-#include <boost/assign/v2/ref/wrapper/copy.hpp>
-#include <boost/assign/v2/ref/wrapper/cpp0x.hpp>
-#include <boost/assign/v2/ref/wrapper/get.hpp>
-#include <boost/assign/v2/ref/wrapper/range_get.hpp>
+#include <boost/assign/v2/put/pipe/functor.hpp>
+#include <boost/assign/v2/put/pipe/csv.hpp>
+#include <boost/assign/v2/put/pipe/range.hpp>
 
-#endif
+#endif // BOOST_ASSIGN_V2_PUT_PIPE_ER_2010_HPP
