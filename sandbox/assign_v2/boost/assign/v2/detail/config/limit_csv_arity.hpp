19d892499cbde75c5c404b6f530a5b350bc0e635
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-08 16:35:36 +0000

    upd assign_v2

--- sandbox/assign_v2/boost/assign/v2/detail/config/limit_csv_arity.hpp
+++ sandbox/assign_v2/boost/assign/v2/detail/config/limit_csv_arity.hpp
@@ -7,26 +7,9 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_V2_DETAIL_CHECK_EQUAL_CONTAINER_RANGE_ER_2010_HPP
-#define BOOST_ASSIGN_V2_DETAIL_CHECK_EQUAL_CONTAINER_RANGE_ER_2010_HPP
-#include <boost/range/algorithm/equal.hpp>
-#include <boost/assign/v2/detail/config/check.hpp>
-#include <boost/assign/v2/detail/traits/container/is_range.hpp>
 
-namespace boost{
-namespace assign{
-namespace v2{
-namespace check_aux{
-
-    template<typename C, typename R>
-    void equal_container(v2::container_tag::range, C const & cont, R const& r)
-    {
-        BOOST_ASSIGN_V2_CHECK( boost::equal( cont, r ) );
-    }
-
-}// check_aux
-}// v2
-}// assign
-}// boost
-
-#endif // BOOST_ASSIGN_V2_DETAIL_CHECK_EQUAL_CONTAINER_RANGE_ER_2010_HPP
+#ifndef BOOST_ASSIGN_V2_LIMIT_CSV_ARITY
+//[limit_csv_arity
+#define BOOST_ASSIGN_V2_LIMIT_CSV_ARITY 20
+//]
+#endif // BOOST_ASSIGN_V2_LIMIT_CSV_ARITY
