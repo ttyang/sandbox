aadfec8f6b51f34e88c2f39f7cd97a7c9b1d0337
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-21 00:11:59 +0000

    upd assign_v2

---
+++ sandbox/assign_v2/libs/assign/v2/test/value/pipe.cpp
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#include <libs/assign/v2/test/value/pipe/args_list.h>
+#include <libs/assign/v2/test/value/pipe/fun.h>
+#include <libs/assign/v2/test/value/pipe/modifier.h>
+#include <libs/assign/v2/test/value/pipe/put.h>
+#include <libs/assign/v2/test/value/pipe.h>
+
+namespace test_assign_v2{
+namespace xxx_value{
+namespace xxx_pipe{
+
+    void test()
+    {
+    	// TODO
+        //xxx_args_list::test();
+        //xxx_fun::test();
+        //xxx_modifier::test();
+        xxx_put::test();
+    }
+
+}// xxx_pipe
+}// xxx_value
+}// xxx_test_assign
+
