2dd542aa0ce78a995f6e3b99035ea23ed6110cb9
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-22 19:56:01 +0000

    upd assign_v2

--- sandbox/assign_v2/boost/assign/v2/option/std_modifier/include.hpp
+++ sandbox/assign_v2/boost/assign/v2/option/std_modifier/include.hpp
@@ -7,10 +7,11 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_V2_OPTION_PUSH_ER_2010_HPP
-#define BOOST_ASSIGN_V2_OPTION_PUSH_ER_2010_HPP
-#include <boost/assign/v2/option/std_modifier/modifier.hpp>
-BOOST_ASSIGN_V2_OPTION_STD_MODIFIER_TAG(push)
-BOOST_ASSIGN_V2_OPTION_STD_MODIFIER(push)
+#ifndef BOOST_ASSIGN_V2_OPTION_STD_MODIFIER_INCLUDE_ER_2010_HPP
+#define BOOST_ASSIGN_V2_OPTION_STD_MODIFIER_INCLUDE_ER_2010_HPP
+#include <boost/assign/v2/option/insert.hpp>
+#include <boost/assign/v2/option/push.hpp>
+#include <boost/assign/v2/option/push_back.hpp>
+#include <boost/assign/v2/option/push_front.hpp>
 
-#endif // BOOST_ASSIGN_V2_OPTION_PUSH_ER_2010_HPP
+#endif // BOOST_ASSIGN_V2_OPTION_STD_MODIFIER_INCLUDE_ER_2010_HPP
