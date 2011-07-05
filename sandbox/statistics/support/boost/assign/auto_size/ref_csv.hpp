0e83eb138ab277c1eaa6d90c6e499cdfa04569c1
Erwann Rogard <erwann.rogard@gmail.com>
2011-07-05 11:29:47 +0000

    cleaning up directory

--- sandbox/statistics/support/boost/assign/auto_size/ref_csv.hpp
+++ sandbox/statistics/support/boost/assign/auto_size/ref_csv.hpp
@@ -1,19 +1,23 @@
 //////////////////////////////////////////////////////////////////////////////
-// assign::auto_size.hpp                                                    //
+// assign::ref_csv.hpp                                                      //
 //                                                                          //
 //  Copyright (C) 2010 Erwann Rogard                                        //
 //  Use, modification and distribution are subject to the                   //
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_AUTO_SIZE_ER_2010_HPP
-#define BOOST_ASSIGN_AUTO_SIZE_ER_2010_HPP
+#ifndef BOOST_ASSIGN_AUTO_SIZE_REF_CSV_ER_2010_HPP
+#define BOOST_ASSIGN_AUTO_SIZE_REF_CSV_ER_2010_HPP
 
-#include <boost/assign/auto_size/ref_list_of.hpp> 
-#include <boost/assign/auto_size/ref_rebind_list_of.hpp> 
-#include <boost/assign/auto_size/ref_csv.hpp> 
-#include <boost/assign/auto_size/ref_rebind_csv.hpp> 
-#include <boost/assign/auto_size/range/chain_l.hpp> 
-#include <boost/assign/auto_size/range/chain_r.hpp>
- 
+// Usage:
+// ref_csv(a,b,c);
+// cref_csv(a,b,c);
+// ref_csv<P>(a,b,c);
+// cref_csv<P>(a,b,c);
+
+#define BOOST_ASSIGN_CSV_ref boost::assign::detail::auto_size::ref_copy
+#include <boost/assign/auto_size/detail/csv.hpp>
+BOOST_ASSIGN_CSV(ref_csv)
+#undef BOOST_ASSIGN_CSV_ref
+
 #endif
