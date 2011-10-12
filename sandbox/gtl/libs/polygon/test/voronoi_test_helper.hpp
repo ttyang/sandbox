d3196c7f92d28c76321045b15b7f30221b46637d
Andrii Sydorchuk <sydorchuk.andriy@gmail.com>
2011-10-12 13:53:34 +0000

    Minor fix to compile on Linux.

--- sandbox/gtl/libs/polygon/test/voronoi_test_helper.hpp
+++ sandbox/gtl/libs/polygon/test/voronoi_test_helper.hpp
@@ -212,8 +212,8 @@ void save_voronoi_input(const std::vector< point_data<T> > &points, const char *
 template <typename T>
 void save_voronoi_input(const directed_line_segment_set_data<T> &segments, const char *file_name) {
     std::ofstream ofs(file_name);
-    //ofs << segments.size() << std::endl;
-    for (directed_line_segment_set_data<T>::iterator_type it = segments.begin();
+    ofs << segments.size() << std::endl;
+    for (typename directed_line_segment_set_data<T>::iterator_type it = segments.begin();
          it != segments.end(); ++it) {
         ofs << it->low().x() << " " << it->low().y() << " ";
         ofs << it->high().x() << " " << it->high().y() << std::endl;
