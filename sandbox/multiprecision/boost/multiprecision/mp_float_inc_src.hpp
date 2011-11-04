3dec1ad54c8686ab371d401165beea8ad8f10b97
Paul A. Bristow <pbristow@hetp.u-net.com>
2011-11-04 12:17:25 +0000

    Add file containing basic includes.

---
+++ sandbox/multiprecision/boost/multiprecision/mp_float_inc_src.hpp
@@ -0,0 +1,19 @@
+
+// Copyright Paul A. Bristow  2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+// Include the basic source files for mp_float.
+
+#include <libs/multiprecision/src/backends/float/mp_float.cpp>
+#include <libs/multiprecision/src/backends/float/mp_float_base.cpp>
+#include <libs/multiprecision/src/backends/float/efx/mp_float_efx.cpp>
+
+#include <libs/multiprecision/src/functions/constants/constants.cpp>
+#include <libs/multiprecision/src/functions/elementary/elementary_complex.cpp>
+#include <libs/multiprecision/src/functions/elementary/elementary_hyper_g.cpp>
+#include <libs/multiprecision/src/functions/elementary/elementary_math.cpp>
+#include <libs/multiprecision/src/functions/elementary/elementary_trans.cpp>
+#include <libs/multiprecision/src/functions/elementary/elementary_trig.cpp>
+
