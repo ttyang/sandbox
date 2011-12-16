a45fdccbe80d5b5be4e3b33fd9401e0849f0b2db
Lorenzo Caminiti <lorcaminiti@gmail.com>
2011-12-16 11:59:32 +0000

    Ranming pp-variadics.

---
+++ sandbox/closure/boost/closure/aux_/preprocessor/variadic/to_seq.hpp
@@ -0,0 +1,43 @@
+
+// Copyright (C) 2009-2011 Lorenzo Caminiti
+// Use, modification, and distribution is subject to the Boost Software
+// License, Version 1.0 (see accompanying file LICENSE_1_0.txt or a
+// copy at http://www.boost.org/LICENSE_1_0.txt).
+
+#ifndef BOOST_CLOSURE_AUX_PP_VARIADIC_TO_SEQ_HPP_
+#define BOOST_CLOSURE_AUX_PP_VARIADIC_TO_SEQ_HPP_
+
+#include <boost/config.hpp>
+
+#if !defined(BOOST_NO_VARIADIC_MACROS) // If no variadics then no macros.
+
+#include <boost/closure/aux_/preprocessor/variadic/is.hpp>
+#include <boost/preprocessor/variadic/to_seq.hpp>
+#include <boost/preprocessor/control/iif.hpp>
+#include <boost/preprocessor/control/if.hpp>
+
+// PRIVATE //
+
+#define BOOST_CLOSURE_AUX_PP_VARIADIC_TO_SEQ_NOT_EMPTY_(...) \
+    BOOST_PP_IIF(BOOST_CLOSURE_AUX_PP_IS_VARIADIC(__VA_ARGS__), \
+        BOOST_PP_VARIADIC_TO_SEQ \
+    , \
+        BOOST_PP_REM \
+    )(__VA_ARGS__)
+
+// PUBLIC //
+
+// Expand to `(arg0)(arg1)...` is __VA_ARGS__ is `arg0, arg1, ...` or already
+// `(arg0)(arg1)...`, if __VA_ARGS__ is empty `` expand to `empty_seq`.
+#define BOOST_CLOSURE_AUX_PP_VARIADIC_TO_SEQ(empty_seq, ...) \
+    BOOST_PP_IF(BOOST_CLOSURE_AUX_PP_VARIADIC_SIZE(__VA_ARGS__), \
+        BOOST_CLOSURE_AUX_PP_VARIADIC_TO_SEQ_NOT_EMPTY_ \
+    , \
+        empty_seq BOOST_PP_EAT \
+    )(__VA_ARGS__)
+
+#endif // BOOST_NO_VARIADIC_MACROS
+
+#endif // #include guard
+
+
