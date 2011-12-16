5f2c9d13c45e1aae07edeb855aafc48e9a376741
Joachim Faulhaber <afojgo@gmail.com>
2011-12-16 15:16:48 +0000

    Changes on the toy db-schema

--- sandbox/icl/libs/xplore/br1/sqlbrowser/MakeMedia1.sql
+++ sandbox/icl/libs/xplore/br1/sqlbrowser/MakeMedia1.sql
@@ -7,12 +7,37 @@ create table Albums (id integer primary key, Title varchar, Artist varchar, Genr
 insert into Albums values (1, 'Kind of Blue', 'Miles Davis', 'Jazz', '9.3');
 insert into Albums values (2, 'Nefertiti',    'Miles Davis', 'Jazz', '9.1');
 
-create table Tracks (id integer primary key, Album integer, Pos integer, Title varchar, Artist varchar, Genre varchar, Rating number, Duration varchar, BPM number);
+create table Tracks (id integer primary key, Title varchar, Artist varchar, Genre varchar, Rating number, Duration varchar, BPM number);
 
-insert into Tracks values (1, 1, 1, 'So What', 'Miles Davis', 'Jazz', '9.6', '09:05', '80');
-insert into Tracks values (2, 1, 2, 'Freddie Freeloader', 'Miles Davis', 'Jazz', '9.3', '09:35', '');
+-- Kind of Blue
+insert into Tracks values (1, 'So What', 'Miles Davis', 'Jazz', '9.6', '09:05', '80');
+insert into Tracks values (2, 'Freddie Freeloader', 'Miles Davis', 'Jazz', '9.3', '09:35', '');
+-- Nefertiti
+insert into Tracks values (3, 'Nefertiti', 'Wayne Shorter', 'Jazz', '9.0', '07:52', '80');
+insert into Tracks values (4, 'Fall',      'Wayne Shorter', 'Jazz', '9.3', '06:39', '');
 
-create table AlbumsByTracks (id integer primary key, Album integer, Track integer, Pos integer);
+
+create table AlbumsByTracks (Album integer, Track integer, Pos integer);
+
+insert into AlbumsByTracks values (1, 1, 1);
+insert into AlbumsByTracks values (1, 2, 2);
+insert into AlbumsByTracks values (2, 3, 1);
+insert into AlbumsByTracks values (2, 4, 2);
+
+
+-- ----------------------------------------------------------------------------
+select Albums.Title, Albums.Genre, Tracks.Title, Tracks.Duration, Tracks.Artist
+from AlbumsByTracks, Albums, Tracks
+where AlbumsByTracks.Album = Albums.Id and AlbumsByTracks.Track = Tracks.Id
+
+-- ----------------------------------------------------------------------------
+select Albums.Title, Albums.Genre, Tracks.Title, Tracks.Duration, Tracks.Artist
+from AlbumsByTracks, Albums, Tracks
+inner join Albums on AlbumsByTracks.Album = Albums.Id
+inner join Tracks on AlbumsByTracks.Track = Tracks.Id
+where AlbumsByTracks.Album = Albums.Id and AlbumsByTracks.Track = Tracks.Id
+
+
 
 -- ----------------------------------------------------------------------------
 -- select .. from Refering inner join Target on Refering.Pointer = Target.Id
