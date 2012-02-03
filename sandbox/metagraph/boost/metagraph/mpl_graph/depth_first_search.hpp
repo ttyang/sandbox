ccdf4ab79d08ced81b680176c6e7ad2ba5c1f778
Gordon Woodhull <gordon@woodhull.com>
2012-02-03 05:46:33 +0000

    search iterators

--- sandbox/metagraph/boost/metagraph/mpl_graph/depth_first_search.hpp
+++ sandbox/metagraph/boost/metagraph/mpl_graph/depth_first_search.hpp
@@ -12,110 +12,222 @@
 #include <boost/mpl/pair.hpp>
 #include <boost/mpl/map.hpp>
 #include <boost/mpl/has_key.hpp>
+#include <boost/mpl/pop_front.hpp>
+#include <boost/mpl/empty.hpp>
+#include <boost/mpl/quote.hpp>
+#include <boost/mpl/bool.hpp>
 
 #include "search_colors.hpp"
 
 namespace boost {
-namespace metagraph {
-namespace mpl_graph {
+  namespace metagraph {
+    namespace mpl_graph {
 
-// dfs takes a visitor which has all the bgl-like metafunctions encapsulated in an 
-// "operations" member class, and also a state.  the operations are expected to return a new state
-// in addition, the visitor operations are expected to update the colors of vertices
-// and need to support a new metafunction get_color<Vertex, State>
+      // dfs takes a visitor which has all the bgl-like metafunctions encapsulated in an 
+      // "operations" member class, and also a state.  the operations are expected to return a new state
+      // in addition, the visitor operations are expected to update the colors of vertices
+      // and need to support a new metafunction get_color<Vertex, State>
 
-struct dfs_default_visitor_operations {
-    template<typename Vertex, typename Graph, typename State>
-    struct initialize_vertex {
-        typedef State type;       
-    };
+      struct dfs_default_visitor_operations {
+        template<typename Vertex, typename Graph, typename State>
+        struct initialize_vertex {
+          typedef State type;       
+        };
     
-    template<typename Vertex, typename Graph, typename State>
-    struct discover_vertex {
-        typedef State type;       
-    };
+        template<typename Vertex, typename Graph, typename State>
+        struct discover_vertex {
+          typedef State type;       
+        };
     
-    template<typename Vertex, typename Graph, typename State>
-    struct finish_vertex {
-        typedef State type;       
-    };
+        template<typename Vertex, typename Graph, typename State>
+        struct finish_vertex {
+          typedef State type;       
+        };
         
-    template<typename Edge, typename Graph, typename State>
-    struct tree_edge {
-        typedef State type;
-    };
+        template<typename Edge, typename Graph, typename State>
+        struct tree_edge {
+          typedef State type;
+        };
     
-    template<typename Edge, typename Graph, typename State>
-    struct back_edge {
-        typedef State type;
-    };
+        template<typename Edge, typename Graph, typename State>
+        struct back_edge {
+          typedef State type;
+        };
     
-    template<typename Edge, typename Graph, typename State>
-    struct forward_or_cross_edge {
-        typedef State type;
-    };  
-};
+        template<typename Edge, typename Graph, typename State>
+        struct forward_or_cross_edge {
+          typedef State type;
+        };  
+      };
 
-// requires IncidenceGraph
-// returns pair<VisitorState, ColorState>
-template<typename Graph, typename VisitorOps, typename VisitorState,
-         typename Vertex, 
-         typename ColorState = create_search_color_map::type >
-struct depth_first_search {
-    // enter vertex
-    typedef typename VisitorOps::template 
-        discover_vertex<Vertex, Graph, VisitorState>::type 
-            discovered_state;
-    typedef typename search_color_map_ops::template 
-        set_color<Vertex, search_colors::Gray, ColorState>::type 
-            discovered_colors;
+      namespace detail {
+        template<typename Item, typename IsVertex, typename IsNew>
+        struct dfs_frame {
+          typedef Item item;
+          typedef IsVertex is_vertex;
+          typedef IsNew is_new;
+        };
+        template<typename Item, typename IsVertex, typename IsNew>
+        struct make_dfs_frame {
+          typedef dfs_frame<Item, IsVertex, IsNew> type; // surely there's an easier way to do this?
+        };
+
+        template<typename Graph, typename VisitorOps, typename Stack,
+                 typename VisitorState, typename ColorMap, typename Vertex>
+        struct dfs_step_in_vertex {
+          // enter vertex
+          typedef typename VisitorOps::template 
+          discover_vertex<Vertex, Graph, VisitorState>::type 
+          discovered_state;
+          // color it
+          typedef typename search_color_map_ops::template 
+          set_color<Vertex, search_colors::Gray, ColorMap>::type 
+          discovered_colors;
+
+          // put exit frame on stack
+          typedef typename mpl::push_front<Stack, dfs_frame<Vertex, mpl::true_, mpl::false_> >::type stack;
             
-    // loop over out edges
-    typedef typename 
-        mpl::fold<typename mpl_graph::out_edges<Vertex, Graph>::type, 
-                  mpl::pair<discovered_state, discovered_colors>,
-                  mpl::if_<boost::is_same<search_color_map_ops::get_color<mpl_graph::target<mpl::_2, Graph>, mpl::second<mpl::_1> >, 
-                                          search_colors::White>,
-                           // unseen target: recurse
-                           depth_first_search<Graph, 
-                                              VisitorOps, typename VisitorOps::template tree_edge<mpl::_2, Graph, mpl::first<mpl::_1> >, 
-                                              mpl_graph::target<mpl::_2, Graph>, 
-                                              mpl::second<mpl::_1> >,
-                           // seen: back or forward edge
-                           mpl::pair<mpl::if_<boost::is_same<typename search_color_map_ops::template 
-                                                 get_color<mpl_graph::target<mpl::_2, Graph>, mpl::second<mpl::_1 > >, 
-                                                 search_colors::Gray>,
-                                              typename VisitorOps::template back_edge<mpl::_2, Graph, mpl::first<mpl::_1> >,
-                                              typename VisitorOps::template forward_or_cross_edge<mpl::_2, Graph, mpl::first<mpl::_1> > >,  // Black
-                                     mpl::second<mpl::_1> > > 
-                 >::type after_outedges;
-                 
-    // leave vertex, and done!
-    typedef mpl::pair<typename VisitorOps::template finish_vertex<Vertex, Graph, typename mpl::first<after_outedges>::type >::type,
-                      typename search_color_map_ops::template set_color<Vertex, search_colors::Black, typename mpl::second<after_outedges>::type>::type> type;
-};
+          // loop over out edges
+          typedef mpl::vector<typename 
+                              mpl::reverse_fold<typename 
+                                        mpl_graph::out_edges<Vertex, Graph>::type, 
+                                        stack,
+                                        mpl::push_front<mpl::_1, make_dfs_frame<mpl::_2, mpl::false_, mpl::false_> > >::type, 
+                              discovered_state, discovered_colors> type;
+        };
+        template<typename Graph, typename VisitorOps, typename Stack,
+                 typename VisitorState, typename ColorMap, typename Vertex>
+        struct dfs_step_out_vertex {
+          typedef mpl::vector<Stack,
+                              typename VisitorOps::template finish_vertex<Vertex, Graph, VisitorState>::type,
+                              typename search_color_map_ops::template set_color<Vertex, search_colors::Black, ColorMap>::type> type;
+        };
+        template<typename Graph, typename VisitorOps, typename Stack,
+                 typename VisitorState, typename ColorMap, typename Edge, typename Target>
+        struct dfs_unseen_target :
+          mpl::vector<typename mpl::push_front<Stack, dfs_frame<Target, mpl::true_, mpl::true_> >::type,
+                      typename VisitorOps::template tree_edge<Edge, Graph, VisitorState >::type, 
+                      ColorMap> {};
+        template<typename Graph, typename VisitorOps, typename Stack,
+                 typename VisitorState, typename ColorMap, typename Edge, typename Color>
+        struct dfs_seen_target :
+          mpl::vector<Stack,
+                      typename
+                      mpl::eval_if<typename boost::is_same<Color, search_colors::Gray>::type,
+                                   typename VisitorOps::template back_edge<Edge, Graph, VisitorState >,
+                                   typename VisitorOps::template forward_or_cross_edge<Edge, Graph, VisitorState > // Black
+                                   >::type, 
+                      ColorMap> {};
+        template<typename Graph, typename VisitorOps, typename Stack,
+                 typename VisitorState, typename ColorMap, typename Edge>
+        struct dfs_step_edge {
+          typedef typename mpl_graph::target<Edge, Graph>::type target;
+          typedef typename search_color_map_ops::get_color<target, ColorMap >::type color;
+          typedef typename 
+          mpl::eval_if<typename boost::is_same<color, search_colors::White>::type,
+                       // unseen target: put it on stack
+                       dfs_unseen_target<Graph, VisitorOps, Stack, VisitorState, ColorMap, Edge, target>,
+                       // seen: back or forward edge
+                       dfs_seen_target<Graph, VisitorOps, Stack, VisitorState, ColorMap, Edge, color>
+                        >::type type;
+        };
+                                     
+          
+        template<typename T> struct err_it;
+         
+        template<typename Frame, typename Graph, typename VisitorOps, typename Stack,
+                 typename VisitorState, typename ColorMap>
+        struct dispatch_frame;
 
-// requires IncidenceGraph, VertexListGraph
-template<typename Graph, typename VisitorOps, typename VisitorState,
-         typename FirstVertex = typename mpl::front<typename mpl_graph::vertices<Graph>::type>::type,
-         typename ColorState = create_search_color_map::type>
-struct depth_first_search_all : // visit first then rest
-    mpl::fold<typename mpl_graph::vertices<Graph>::type,
-              typename depth_first_search<Graph, 
-                                          VisitorOps, VisitorState, 
-                                          FirstVertex, 
-                                          ColorState>::type, 
-              mpl::if_<boost::is_same<search_color_map_ops::get_color<mpl::_2, mpl::second<mpl::_1> >,
-                                      search_colors::White>, // visit any yet unvisited
-                       depth_first_search<Graph,
-                                 VisitorOps, mpl::first<mpl::_1>,
-                                 mpl::_2,
-                                 mpl::second<mpl::_1> >,
-                       mpl::_1> >   
-{};
 
-} // namespace mpl_graph
-} // namespace metagraph
+        template<typename Item, typename Graph, typename VisitorOps, typename Stack,
+               typename VisitorState, typename ColorMap>
+        struct dispatch_frame<dfs_frame<Item, mpl::true_, mpl::true_>, 
+                              Graph, VisitorOps, Stack,
+                              VisitorState, ColorMap> :
+        dfs_step_in_vertex<Graph, VisitorOps, Stack,
+                           VisitorState, ColorMap, Item> {};
+ 
+
+        template<typename Item, typename Graph, typename VisitorOps, typename Stack,
+               typename VisitorState, typename ColorMap>
+        struct dispatch_frame<dfs_frame<Item, mpl::true_, mpl::false_>, 
+                              Graph, VisitorOps, Stack,
+                              VisitorState, ColorMap> :
+        dfs_step_out_vertex<Graph, VisitorOps, Stack,
+                            VisitorState, ColorMap, Item> {};
+        
+
+        template<typename Item, typename Graph, typename VisitorOps, typename Stack,
+                 typename VisitorState, typename ColorMap, typename Unused>
+        struct dispatch_frame<dfs_frame<Item, mpl::false_, Unused>, 
+                              Graph, VisitorOps, Stack,
+                              VisitorState, ColorMap> :
+        dfs_step_edge<Graph, VisitorOps, Stack,
+                      VisitorState, ColorMap, Item> {};
+ 
+        template<typename Graph, typename VisitorOps, typename Stack,
+                 typename VisitorState, typename ColorMap>
+        struct dfs_step {
+          typedef typename mpl::pop_front<Stack>::type stack;
+          typedef typename mpl::front<Stack>::type frame;
+          //typedef typename err_it<frame>::type foo;
+          typedef typename dispatch_frame<frame, Graph, VisitorOps, stack, VisitorState, ColorMap>::type type;
+        };
+        
+        struct dfs_end {};
+        template<typename Graph, typename VisitorOps, typename Stack,
+                 typename VisitorState, typename ColorMap>
+        struct dfs_iter {
+          typedef typename dfs_step<Graph, VisitorOps, Stack, VisitorState, ColorMap>::type eval;
+          typedef typename mpl::at_c<eval, 0>::type stack;
+          typedef typename mpl::at_c<eval, 1>::type visitor_state;
+          typedef typename mpl::at_c<eval, 2>::type color_map;
+
+          typedef typename mpl::if_<typename mpl::empty<stack>::type,
+                                    dfs_end,
+                                    dfs_iter<Graph, VisitorOps, stack, visitor_state, color_map> >::type next;
+          typedef mpl::pair<visitor_state, color_map> type;
+        };
+        template<typename BeginIter>
+        struct dfs_sequence {
+          typedef BeginIter begin;
+          typedef dfs_end end;
+        };
+      }
+      // requires IncidenceGraph
+      // returns pair<VisitorState, ColorMap>
+      template<typename Graph, typename VisitorOps, typename VisitorState,
+               typename Vertex, 
+               typename ColorMap = create_search_color_map::type >
+      struct depth_first_search {
+        typedef mpl::vector<detail::dfs_frame<Vertex, mpl::true_, mpl::true_> > stack;
+        typedef typename mpl::fold<detail::dfs_sequence<detail::dfs_iter<Graph, VisitorOps, stack, VisitorState, ColorMap> >,
+                                   void,
+                                   mpl::_2>::type type;
+      };
+
+      // requires IncidenceGraph, VertexListGraph
+      template<typename Graph, typename VisitorOps, typename VisitorState,
+               typename FirstVertex = typename mpl::front<typename mpl_graph::vertices<Graph>::type>::type,
+               typename ColorMap = create_search_color_map::type>
+      struct depth_first_search_all : // visit first then rest
+      mpl::fold<typename mpl_graph::vertices<Graph>::type,
+                typename depth_first_search<Graph, 
+                                            VisitorOps, VisitorState, 
+                                            FirstVertex, 
+                                            ColorMap>::type, 
+                mpl::if_<boost::is_same<search_color_map_ops::get_color<mpl::_2, mpl::second<mpl::_1> >,
+                                        search_colors::White>, // visit any yet unvisited
+                         depth_first_search<Graph,
+                                            VisitorOps, mpl::first<mpl::_1>,
+                                            mpl::_2,
+                                            mpl::second<mpl::_1> >,
+                         mpl::_1> >   
+      {};
+
+    } // namespace mpl_graph
+  } // namespace metagraph
 } // namespace boost
 
 
