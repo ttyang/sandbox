4de13dcf526833cb1293aa9fc9d3ba3a760b27d7
Andrii Sydorchuk <sydorchuk.andriy@gmail.com>
2012-02-13 23:05:17 +0000

    Code styling and comments refactoring.

--- sandbox/gtl/boost/polygon/voronoi.hpp
+++ sandbox/gtl/boost/polygon/voronoi.hpp
@@ -13,46 +13,48 @@
 #include "voronoi_builder.hpp"
 #include "voronoi_diagram.hpp"
 
+// Public methods to compute voronoi diagram.
+// points - container of input points (supports forward iterator).
+// segments - container of input segments (supports forward iterator).
+// output - voronoi output data structure to hold voronoi diagram.
+// The assumption is made that input doesn't contain segments that intersect
+// or points lying on the segments. Also coordinates of the points and of the
+// endpoints of the segments should belong to the signed integer range
+// [-2^31, 2^31-1]. To use wider input coordinate range use voronoi_builder
+// structure with user provided coordinate traits.
+// Complexity - O(N*logN), memory usage - O(N),
+// where N is the total number of points and segments.
 namespace boost {
 namespace polygon {
-    // Public methods to compute voronoi diagram.
-    // points - vector of input points.
-    // segments - vector of input segments.
-    // output - voronoi output data structure to hold voronoi diagram.
-    // The assumption is made that input doesn't contain segments that
-    // intersect or points lying on the segments. Also coordinates of
-    // the points and of the endpoints of the segments should belong to
-    // the signed integer range [-2^31, 2^31-1].
-    // Complexity - O(N*logN), memory usage - O(N), where N is the total
-    // number of points and segments.
-    template <typename PC, typename VD>
-    static inline void construct_voronoi_points(
-        const PC &points, VD *output) {
-        default_voronoi_builder builder;
-        builder.insert_points(points.begin(), points.end());
-        builder.construct(output);
-        builder.clear();
-    }
 
-    template <typename SC, typename VD>
-    static inline void construct_voronoi_segments(
-        const SC &segments, VD *output) {
-        default_voronoi_builder builder;
-        builder.insert_segments(segments.begin(), segments.end());
-        builder.construct(output);
-        builder.clear();
-    }
+template <typename PC, typename VD>
+static inline void construct_voronoi_points(
+    const PC &points, VD *output) {
+  default_voronoi_builder builder;
+  builder.insert_points(points.begin(), points.end());
+  builder.construct(output);
+  builder.clear();
+}
 
-    template <typename PC, typename SC, typename VD>
-    static inline void construct_voronoi(
-        const PC &points, const SC &segments, VD *output) {
-        default_voronoi_builder builder;
-        builder.insert_sites(points.begin(), points.end(),
-                             segments.begin(), segments.end());
-        builder.construct(output);
-        builder.clear();
-    }
+template <typename SC, typename VD>
+static inline void construct_voronoi_segments(
+    const SC &segments, VD *output) {
+  default_voronoi_builder builder;
+  builder.insert_segments(segments.begin(), segments.end());
+  builder.construct(output);
+  builder.clear();
+}
+
+template <typename PC, typename SC, typename VD>
+static inline void construct_voronoi(
+    const PC &points, const SC &segments, VD *output) {
+  default_voronoi_builder builder;
+  builder.insert_sites(
+    points.begin(), points.end(), segments.begin(), segments.end());
+  builder.construct(output);
+  builder.clear();
+}
 }
 }
 
-#endif
+#endif  // BOOST_POLYGON_VORONOI
