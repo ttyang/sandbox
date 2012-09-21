79d6e2493761c32d9209c6ea835285a7935d0776
Larry Joe Evans <cppljevans@suddenlink.net>
2012-09-21 14:02:58 +0000

    Missing include file used by tuple.benchmark.cpp

---
+++ sandbox/variadic_templates/sandbox/slim/test/tuple_test_impl_macros.hpp
@@ -0,0 +1,7 @@
+#ifndef TUPLE_TEST_IMPL_MACROS_HPP_INCLUDED
+#define TUPLE_TEST_IMPL_MACROS_HPP_INCLUDED
+//Purpose:
+//  Define PP macros used to number tuple implementations.
+#define TUPLE_TEST_HORIZONTAL 0
+#define TUPLE_TEST_VERTICAL (TUPLE_TEST_HORIZONTAL+1)
+#endif
