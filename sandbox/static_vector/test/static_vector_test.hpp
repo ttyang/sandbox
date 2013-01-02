4d50f15ab38571e81249a125aade6b500b58585a
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2013-01-02 18:20:58 +0000

    static_vector test files renamed.

--- sandbox/static_vector/test/static_vector_test.hpp
+++ sandbox/static_vector/test/static_vector_test.hpp
@@ -8,8 +8,8 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_TEST_STATIC_VECTOR_HPP
-#define BOOST_TEST_STATIC_VECTOR_HPP
+#ifndef BOOST_CONTAINER_TEST_STATIC_VECTOR_TEST_HPP
+#define BOOST_CONTAINER_TEST_STATIC_VECTOR_TEST_HPP
 
 #include <boost/container/static_vector.hpp>
 
@@ -111,4 +111,4 @@ private:
     boost::shared_ptr<int> m_ptr;
 };
 
-#endif // BOOST_TEST_STATIC_VECTOR_HPP
+#endif // BOOST_CONTAINER_TEST_STATIC_VECTOR_TEST_HPP
